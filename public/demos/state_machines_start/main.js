3e39c0682fcb6907b0cee7fd9bc1958f8e6402e2
dustinpfister <dustin.pfister@gmail.com>
2017-10-01 13:24:21 -0400

    first demo working

--- public/demos/state_machines_start/main.js
+++ public/demos/state_machines_start/main.js
@@ -1,1 +1,34 @@
 console.log('main.js');
+var bx, i = 0, maxI = 100;
+var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
+
+        create : function () {
+
+            console.log(game.add);
+
+            bx = game.add.graphics(0, 0);
+
+            bx.beginFill(0x00ff00);
+            bx.drawCircle(50, 50, 100);
+            bx.endFill();
+
+        },
+
+        update : function () {
+
+            var r = Math.PI * 2 / maxI * i;
+
+            bx.x = game.world.centerX - 50 + Math.cos(r) * 50;
+            bx.y = game.world.centerY - 50 + Math.sin(r) * 50;
+
+            i += 1;
+
+            if (i === maxI) {
+
+                i = 0;
+
+            }
+
+        }
+
+    });
