6e54288147d5d516b3829630f4c53efe1f936d87
dustinpfister <dustin.pfister@gmail.com>
2017-10-05 09:19:26 -0400

    demo update

--- public/demos/state_machines_manager/main.js
+++ public/demos/state_machines_manager/main.js
@@ -55,54 +55,51 @@ console.log(game.state.states.default.create); // this function
 game.state.start('default');
  */
 
+/*
 var Boot = {
 
-    // create method
-    create : function () {
+// create method
+create : function () {
 
-        console.log('Boot state');
+console.log('Boot state');
 
-        game.state.start('title');
+game.state.start('title');
 
-    }
+}
 
 },
 
 // A Title State
 Title = {
 
-    // create method
-    create : function () {
+// create method
+create : function () {
 
-        console.log('Title state');
+console.log('Title state');
 
-    },
+},
 
-    update : function () {
+update : function () {
 
-        // add some code that is a condition
-        // that will lead to  starting game
-        game.state.start('game');
+// add some code that is a condition
+// that will lead to  starting game
+game.state.start('game');
 
-    }
+}
 
 },
 
 // A Game State
 Game = {
 
-    // create method
-    create : function () {
+// create method
+create : function () {
 
-        console.log('Game state');
+console.log('Game state');
 
-    },
-	
-	update : function(){
-		
-		
-		
-	}
+},
+
+update : function () {}
 
 };
 
@@ -116,3 +113,76 @@ game.state.add('game', Game);
 
 // start With default
 game.state.start('default');
+
+ */
+
+/*
+console.log('yeah');
+console.log(new Phaser.State());
+ */
+
+/*
+var state = new Phaser.State();
+
+console.log(state);
+ */
+
+/*
+var game = new Phaser.Game(320, 240, Phaser.AUTO,'gamearea');
+
+game.state.add('func', function(){
+
+console.log('yes I am just a function');
+
+},true);
+
+console.log(game.state.states.func);
+
+//game.state.start('default');
+ */
+
+var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
+
+game.state.add('setup',
+
+    (function () {
+
+        var box = {};
+
+        // return the state object
+        return {
+
+            // an init method with a param object
+            init : function (param) {
+
+                console.log(param);
+
+                box = {
+
+                    x : param.x || 0,
+                    y : param.y || 0,
+                    dx : param.dx || 1,
+                    dy : param.dy || 0
+
+                }
+
+            },
+
+            // a core update method
+            update : function () {
+
+                box.x += box.dx;
+                box.y += box.dy;
+
+            }
+
+        };
+
+    }
+        ()));
+
+game.state.start('setup', true, false, {
+
+    foo : 'bar'
+
+})
