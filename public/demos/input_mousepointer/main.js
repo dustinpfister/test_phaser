d6123af738c111a58e9bf1d0c39d5b52077674bc
dustinpfister <dustin.pfister@gmail.com>
2017-10-12 12:12:00 -0400

    demo update

--- public/demos/input_mousepointer/main.js
+++ public/demos/input_mousepointer/main.js
@@ -1,9 +1,56 @@
 
+/*
 var game = new Phaser.Game(640, 480, Phaser.AUTO, 'gamearea', {
+
+render : function () {
+
+game.debug.pointer(game.input.mousePointer);
+
+}
+
+});
+ */
+
+var game = new Phaser.Game(640, 480, Phaser.AUTO, 'gamearea', {
+
+        create : function () {
+
+            var st = {
+                fill : '#ffffff',
+                font : '15px courier'
+            };
+
+            game.add.text(20, 20, 'tx0', st);
+            game.add.text(20, 40, 'tx1', st);
+
+        },
+
+        update : function () {
+
+            var pt = game.input.mousePointer,
+            time,
+            tx = game.world.children;
+
+            if (pt.isDown) {
+
+                // time can be found thanks to pt.timeDown
+                time = new Date() - pt.timeDown;
+
+                tx[0].text = 'mouse is down!';
+                tx[1].text = time;
+
+            } else {
+
+                tx[0].text = 'mouse is not down.';
+                tx[1].text = '';
+
+            }
+
+        },
 
         render : function () {
 
-            game.debug.pointer(game.input.mousePointer);
+            //game.debug.pointer(game.input.mousePointer);
 
         }
 
