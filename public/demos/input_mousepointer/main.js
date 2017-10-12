6a7c9ca1cb89ae45b3753d361b3cb893d319ad3c
dustinpfister <dustin.pfister@gmail.com>
2017-10-12 11:41:43 -0400

    demo update

--- public/demos/input_mousepointer/main.js
+++ public/demos/input_mousepointer/main.js
@@ -1,30 +1,10 @@
 
-//var tx;
 var game = new Phaser.Game(640, 480, Phaser.AUTO, 'gamearea', {
 
-/*
-        create : function () {
-
-            tx = game.add.text(20, 20, '', {
-                    fill : '#ffffff'
-                });
-
-        },
-
-        update : function () {
-
-            tx.text = game.input.pointers.length;
-
-        },
-*/
         render : function () {
 
-            game.input.pointers.forEach(function (pointer) {
-
-                game.debug.pointer(pointer);
-
-            });
+            game.debug.pointer(game.input.mousePointer);
 
         }
 
-    })
+    });
