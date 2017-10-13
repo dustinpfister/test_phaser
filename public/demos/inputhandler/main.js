ab530af8b204f2f39319c946b49b6d8d7298f056
dustinpfister <dustin.pfister@gmail.com>
2017-10-13 10:16:28 -0400

    demo update

--- public/demos/inputhandler/main.js
+++ public/demos/inputhandler/main.js
@@ -1,94 +1,4 @@
 
-/*
-// the actual App state once everything is loaded
-var App = {
-
-create : function () {
-
-var sprite = game.add.sprite(0, 0, 'zelda');
-sprite.animations.add('walk');
-
-sprite.animations.play('walk', 50, true);
-
-},
-
-update : function () {}
-
-};
-
-// the load state
-var Load = {
-
-preload : function () {
-
-var loadSprite = game.add.sprite(0, 0, 'loadingbar');
-loadSprite.width = 0;
-loadSprite.x = game.world.centerX - loadSprite.width / 2;
-loadSprite.y = game.world.centerY - 16;
-
-game.load.onLoadStart.add(function () {}, this);
-game.load.onFileComplete.add(function (progress) {
-
-loadSprite.width = game.width * (progress / 100);
-loadSprite.x = game.world.centerX - loadSprite.width / 2;
-
-}, this);
-game.load.onLoadComplete.add(function () {}, this);
-
-game.load.image('phaser', '/img/phaser.png');
-game.load.spritesheet('zelda', '/img/sheet_zelda_forse1.png',20,23);
-
-
-},
-
-create : function () {
-
-//app.state.add('title', Title);
-//app.state.add('dig_run', DIG.run);
-//app.state.add('dig_over', DIG.over);
-//app.state.add('dig_options', DIG.options);
-
-//app.state.start('title');
-
-game.state.add('app', App);
-game.state.start('app');
-
-console.log('we be good man');
-
-}
-
-};
-
-// the main game variable
-var game = (function () {
-
-// the main or boot state
-// this is where it all starts
-return new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
-
-preload : function () {
-
-game.load.image('loadingbar', '/img/loadingbar.png');
-
-},
-
-// create method
-create : function () {
-
-game.state.add('load', Load);
-game.state.start('load');
-
-},
-
-// the update method will be called on each tick
-update : function () {}
-
-});
-
-}
-());
- */
-
 var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
 
         // load the sprite sheet
@@ -101,7 +11,10 @@ var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
         // create the sprite
         create : function () {
 
-            game.add.sprite(0, 0, 'cucco');
+            var sprite = game.add.sprite(160 - 40, 120 - 40, 'cucco');
+
+            sprite.width = 80;
+            sprite.height = 80;
 
         },
 
@@ -113,20 +26,35 @@ var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
 
             return function () {
 
-                sprite = game.world.children[0];
+                var sprite = game.world.children[0],
+                pt = game.input.mousePointer;
 
-                sprite.frame = f + 2;
+                if (pt.isDown) {
 
-                if (new Date() - lt > rate) {
+                    var r = Math.atan2(pt.y - sprite.y, pt.x - sprite.x);
 
-                    f += 1;
-                    if (f == 2) {
+                    sprite.x += Math.cos(r);
+                    sprite.y += Math.sin(r);
 
-                        f = 0;
+                    sprite.frame = f + 2;
+
+                    if (new Date() - lt > rate) {
+
+                        f += 1;
+                        if (f == 2) {
+
+                            f = 0;
+
+                        }
+
+                        lt = new Date();
 
                     }
 
-                    lt = new Date();
+                } else {
+
+                    // chicken is at rest state
+                    sprite.frame = 0;
 
                 }
 
@@ -135,4 +63,4 @@ var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
         }
             ())
 
-    });
+    }, false, false);
