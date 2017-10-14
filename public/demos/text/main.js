9e182c43017c309f71d73b29e5b85e1cc7dde190
dustinpfister <dustin.pfister@gmail.com>
2017-10-14 09:13:11 -0400

    demo update

--- public/demos/text/main.js
+++ public/demos/text/main.js
@@ -40,46 +40,61 @@ foos += 1;
 ());
  */
 
+/*
 var game = (function () {
 
-    var tx,
-    i = 0,
-    maxI = 500,
-    per = 0,
-    bias = 0;
+var tx,
+i = 0,
+maxI = 500,
+per = 0,
+bias = 0;
 
-    return new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
+return new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
 
-        // create method
+// create method
+create : function () {
+
+tx = game.add.text(20, 20, '', {
+fill : '#ffffff'
+});
+
+},
+
+// the update method will be called on each tick
+update : function () {
+
+per = i / maxI;
+bias = 1 - Math.abs(.5 - per) / .5;
+
+tx.text = 'i: ' + i;
+
+// changing text position on each update
+tx.x = 150 * bias;
+tx.y = Math.sin(Math.PI * 2 * bias) * 50 + 70;
+
+i += 1;
+if (i >= maxI) {
+
+i = i % maxI;
+
+}
+}
+
+});
+
+}
+());
+ */
+
+var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
+
         create : function () {
 
-            tx = game.add.text(20, 20, '', {
-                    fill : '#ffffff'
-                });
+            game.add.text(0, 0, 'Hello World', {
+                fill : 'white',
+                font : '20px courier'
+            });
 
-        },
-
-        // the update method will be called on each tick
-        update : function () {
-
-            per = i / maxI;
-            bias = 1 - Math.abs(.5 - per) / .5;
-
-            tx.text = 'i: ' + i;
-
-            // changing text position on each update
-            tx.x = 150 * bias;
-            tx.y = Math.sin(Math.PI * 2 * bias) * 50 + 70;
-
-            i += 1;
-            if (i >= maxI) {
-
-                i = i % maxI;
-
-            }
         }
 
     });
-
-}
-    ());
