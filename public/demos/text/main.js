31f773cc5c14542275317d678867750848c1bd11
dustinpfister <dustin.pfister@gmail.com>
2017-10-14 09:06:26 -0400

    demo update

--- public/demos/text/main.js
+++ public/demos/text/main.js
@@ -1,8 +1,52 @@
+/*
+var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
+
+create : function () {
+
+game.add.text(0, 0, 'Hello World', {fill : 'white'});
+
+}
+
+});
+ */
+/*
+var game = (function () {
+
+var tx,
+foos = 0;
+
+return new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
+
+// create method
+create : function () {
+
+tx = game.add.text(20, 20, '', {
+fill : '#ffffff'
+});
+
+},
+
+// the update method will be called on each tick
+update : function () {
+
+tx.text = 'foos: ' + foos;
+
+foos += 1;
+}
+
+});
+
+}
+());
+ */
 
 var game = (function () {
 
     var tx,
-    foos = 0;
+    i = 0,
+    maxI = 500,
+    per = 0,
+    bias = 0;
 
     return new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
 
@@ -18,9 +62,21 @@ var game = (function () {
         // the update method will be called on each tick
         update : function () {
 
-            tx.text = 'foos: ' + foos;
+            per = i / maxI;
+            bias = 1 - Math.abs(.5 - per) / .5;
 
-            foos += 1;
+            tx.text = 'i: ' + i;
+
+            // changing text position on each update
+            tx.x = 150 * bias;
+            tx.y = Math.sin(Math.PI * 2 * bias) * 50 + 70;
+
+            i += 1;
+            if (i >= maxI) {
+
+                i = i % maxI;
+
+            }
         }
 
     });
