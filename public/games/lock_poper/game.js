48fe470e9a83271363a300221e7bd81c1d55d0d6
dustinpfister <dustin.pfister@gmail.com>
2017-10-17 12:35:32 -0400

    target point

--- public/games/lock_poper/game.js
+++ public/games/lock_poper/game.js
@@ -3,9 +3,13 @@ var Game = (function () {
 
     // State variables
     var point, // point display object
+    maxI = 70, // max index for a point
     i = 0, // point index
     di = 1, // delta index
-    maxI = 70, // max index for a point
+
+    target,
+    tarI = 0,
+
     tickRate = 12, // tick rate
     lastTick = new Date(), //the last game tick
 
@@ -18,7 +22,13 @@ var Game = (function () {
 
         //return i % maxI;
 
-        return (i % maxI + maxI) % maxI;
+        return Math.floor((i % maxI + maxI) % maxI);
+
+    },
+
+    newIndex = function (i) {
+
+        return normPTI(i + maxI / 2);
 
     };
 
@@ -36,6 +46,12 @@ var Game = (function () {
             text_index = game.add.text(5, 5, '', style);
 
             // point disp object
+            target = game.add.graphics(0, 0);
+            target.beginFill(0xffff00);
+            target.drawCircle(0, 0, game.world.width / 16);
+            target.endFill();
+
+            // target disp object
             point = game.add.graphics(0, 0);
             point.beginFill(0x00ff00);
             point.drawCircle(0, 0, game.world.width / 16);
@@ -48,6 +64,9 @@ var Game = (function () {
 
             });
 
+            // set a starting target index
+            tarI = newIndex(i);
+
         },
 
         // what to do on each tick
@@ -58,6 +77,10 @@ var Game = (function () {
 
             point.x = game.world.centerX + Math.cos(r) * (game.world.width / 4);
             point.y = game.world.centerY + Math.sin(r) * (game.world.width / 4);
+
+            r = Math.PI * 2 / maxI * tarI
+            target.x = game.world.centerX + Math.cos(r) * (game.world.width / 4);
+            target.y = game.world.centerY + Math.sin(r) * (game.world.width / 4);
 
             text_index.text = 'index: ' + i + '/' + maxI;
 
