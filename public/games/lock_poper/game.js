a702bc638ae2ae6674239a77f478f5ec3d0c5aea
dustinpfister <dustin.pfister@gmail.com>
2017-10-17 12:26:44 -0400

    input, index delta

--- public/games/lock_poper/game.js
+++ public/games/lock_poper/game.js
@@ -2,8 +2,9 @@
 var Game = (function () {
 
     // State variables
-    var pt, // point display object
+    var point, // point display object
     i = 0, // point index
+    di = 1, // delta index
     maxI = 70, // max index for a point
     tickRate = 12, // tick rate
     lastTick = new Date(), //the last game tick
@@ -15,7 +16,9 @@ var Game = (function () {
     // normalize the given point index on a circle
     normPTI = function (i) {
 
-        return i % maxI;
+        //return i % maxI;
+
+        return (i % maxI + maxI) % maxI;
 
     };
 
@@ -33,10 +36,17 @@ var Game = (function () {
             text_index = game.add.text(5, 5, '', style);
 
             // point disp object
-            pt = game.add.graphics(0, 0);
-            pt.beginFill(0x00ff00);
-            pt.drawCircle(0, 0, game.world.width / 16);
-            pt.endFill();
+            point = game.add.graphics(0, 0);
+            point.beginFill(0x00ff00);
+            point.drawCircle(0, 0, game.world.width / 16);
+            point.endFill();
+
+            // input
+            game.input.onDown.add(function (pt, b) {
+
+                di = di === 1 ? -1 : 1;
+
+            });
 
         },
 
@@ -46,13 +56,13 @@ var Game = (function () {
             var r = Math.PI * 2 / maxI * i,
             now = new Date();
 
-            pt.x = game.world.centerX + Math.cos(r) * (game.world.width / 4);
-            pt.y = game.world.centerY + Math.sin(r) * (game.world.width / 4);
+            point.x = game.world.centerX + Math.cos(r) * (game.world.width / 4);
+            point.y = game.world.centerY + Math.sin(r) * (game.world.width / 4);
 
             text_index.text = 'index: ' + i + '/' + maxI;
 
             if (now - lastTick >= tickRate) {
-                i += 1;
+                i += di;
                 i = normPTI(i);
                 lastTick = now;
             }
