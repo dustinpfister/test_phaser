7e29703c94fa3f25f8283bd8efbc187ec27bdc8e
dustinpfister <dustin.pfister@gmail.com>
2017-10-17 12:06:21 -0400

    prototype update

--- public/games/lock_poper/game.js
+++ public/games/lock_poper/game.js
@@ -1,7 +1,11 @@
 // the actual Game state once everything is loaded
 var Game = (function () {
 
-    var pt;
+    var pt,
+    i = 0,
+    maxI = 70,
+    tickRate = 12,
+    lastTick = new Date();
 
     return {
 
@@ -11,15 +15,24 @@ var Game = (function () {
 
             pt = game.add.graphics(0, 0);
             pt.beginFill(0x00ff00);
-            pt.drawCircle(0, 0, 20);
+            pt.drawCircle(0, 0, game.world.width / 16);
             pt.endFill();
 
         },
 
         update : function () {
 
-            pt.x = game.world.centerX;
-            pt.y = game.world.centerY;
+            var r = Math.PI * 2 / maxI * i,
+            now = new Date();
+
+            pt.x = game.world.centerX + Math.cos(r) * (game.world.width / 4);
+            pt.y = game.world.centerY + Math.sin(r) * (game.world.width / 4);
+
+            if (now - lastTick >= tickRate) {
+                i += 1;
+                i = i % maxI;
+                lastTick = now;
+            }
 
         }
 
