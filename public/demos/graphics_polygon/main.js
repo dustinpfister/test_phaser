9816fe22efc69e2f0512976cf1e80c7cb1c39085
dustinpfister <dustin.pfister@gmail.com>
2017-10-23 10:07:23 -0400

    demo update

--- public/demos/graphics_polygon/main.js
+++ public/demos/graphics_polygon/main.js
@@ -4,34 +4,46 @@ var PM = (function () {
 
     var api = {
 
-        p : [],
+        pointCount : 10,
 
+        // the points
+        points : [],
+
+        // parallel array of delta values for the points
+        del : [],
+
+        // set the staring status of points
         setPoints : function () {
 
-            var pCT = 10, // point count
-            pi = 0,
-            p; // current point index
+            // current point index
+            var pi = 0;
 
-            this.p = [];
+            this.points = [];
 
-            p = this.p;
-            while (pi < pCT) {
+            while (pi < this.pointCount) {
 
                 // set some radian, and radius values for each point
-                var ra = Math.PI * 2 / pCT * pi,
+                var ra = Math.PI * 2 / this.pointCount * pi,
                 ri = 75 + Math.random() * 25;
 
                 // push x first, then y
-                p.push(Math.floor(Math.cos(ra) * ri));
-                p.push(Math.floor(y = Math.sin(ra) * ri));
+                this.points.push(Math.floor(Math.cos(ra) * ri));
+                this.points.push(Math.floor(y = Math.sin(ra) * ri));
 
                 pi += 1;
 
             }
 
             // push first point at the end
-            p.push(p[0]);
-            p.push(p[1]);
+            this.points.push(this.points[0]);
+            this.points.push(this.points[1]);
+
+        },
+
+        //
+        setDeltas : function () {
+
+            this.del = [];
 
         }
 
@@ -41,7 +53,6 @@ var PM = (function () {
 
 }
     ());
-
 
 var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
 
@@ -53,21 +64,17 @@ var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
 
             PM.setPoints();
 
-            console.log(PM.p);
-
         },
 
         update : function () {
 
             var gra = game.world.children[0];
 
-			PM.setPoints();
-			
             gra.clear();
 
             gra.lineStyle(3, 0x00ff00);
 
-            gra.drawPolygon(PM.p);
+            gra.drawPolygon(PM.points);
 
         }
 
