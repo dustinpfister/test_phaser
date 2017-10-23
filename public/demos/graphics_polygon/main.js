07fff31106d43c848e8b17db8ec0dec6f08e7e25
dustinpfister <dustin.pfister@gmail.com>
2017-10-23 09:46:09 -0400

    demo update

--- public/demos/graphics_polygon/main.js
+++ public/demos/graphics_polygon/main.js
@@ -1,56 +1,47 @@
 
-/*
-var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
+// Poly Model Example
+var PM = (function () {
 
-// create method
-create : function () {
+    var api = {
 
-// add a graphics object to the world
-var gra = game.add.graphics(game.world.centerX, game.world.centerY);
+        p : [],
 
-//gra.lineStyle(3, 0x00ff00);
+        setPoints : function () {
 
-gra.lineWidth = 3;
-gra.lineColor = 0x00ff00;
-gra.lineAlpha = 1;
+            var pCT = 10, // point count
+            pi = 0,
+            p; // current point index
 
+            this.p = [];
 
-gra.drawPolygon([0, -100, 100, 0, 0, 100, -50, 100, -50, 50, -100, 50, -100, -50, -50, -50, -50, -100, 0, -100]);
+            p = this.p;
+            while (pi < pCT) {
+
+                // set some radian, and radius values for each point
+                var ra = Math.PI * 2 / pCT * pi,
+                ri = 75 + Math.random() * 25;
+
+                // push x first, then y
+                p.push(Math.floor(Math.cos(ra) * ri));
+                p.push(Math.floor(y = Math.sin(ra) * ri));
+
+                pi += 1;
+
+            }
+
+            // push first point at the end
+            p.push(p[0]);
+            p.push(p[1]);
+
+        }
+
+    };
+
+    return api;
 
 }
+    ());
 
-});
- */
-
-var getPoints = function () {
-
-    var pCT = 10, // point count
-    p = [], // points array to be returned
-    pi = 0; // current point index
-    while (pi < pCT) {
-
-        // set some radian, and radius values for each point
-        var ra = Math.PI * 2 / pCT * pi,
-        ri = 75 + Math.random() * 25;
-
-        // push x first, then y
-        p.push(Math.cos(ra) * ri);
-        p.push(y = Math.sin(ra) * ri);
-
-        pi += 1;
-
-    }
-
-    // push first point at the end
-    p.push(p[0]);
-    p.push(p[1]);
-
-    // return the points
-    return p;
-
-},
-
-points = getPoints();
 
 var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
 
@@ -60,19 +51,23 @@ var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
             // add a graphics object to the world
             var gra = game.add.graphics(game.world.centerX, game.world.centerY);
 
+            PM.setPoints();
+
+            console.log(PM.p);
+
         },
 
         update : function () {
 
             var gra = game.world.children[0];
 
-            points = getPoints();
-
+			PM.setPoints();
+			
             gra.clear();
 
             gra.lineStyle(3, 0x00ff00);
 
-            gra.drawPolygon(points);
+            gra.drawPolygon(PM.p);
 
         }
 
