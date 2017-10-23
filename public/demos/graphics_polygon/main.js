267ac39b32b40a4cd4bce4f9458df0f70125744e
dustinpfister <dustin.pfister@gmail.com>
2017-10-23 10:46:13 -0400

    demo update

--- public/demos/graphics_polygon/main.js
+++ public/demos/graphics_polygon/main.js
@@ -10,25 +10,26 @@ var PM = (function () {
         points : [],
 
         // parallel array of delta values for the points
-        del : [],
+        data : [],
 
         // set the staring status of points
         setPoints : function () {
 
             // current point index
-            var pi = 0;
+            var pi = 0,
+            data;
 
             this.points = [];
 
+            // set the values for each point
+            this.setData();
             while (pi < this.pointCount) {
 
-                // set some radian, and radius values for each point
-                var ra = Math.PI * 2 / this.pointCount * pi,
-                ri = 75 + Math.random() * 25;
+                data = this.data[pi];
 
                 // push x first, then y
-                this.points.push(Math.floor(Math.cos(ra) * ri));
-                this.points.push(Math.floor(y = Math.sin(ra) * ri));
+                this.points.push(Math.floor(Math.cos(data.radian) * data.radius));
+                this.points.push(Math.floor(y = Math.sin(data.radian) * data.radius));
 
                 pi += 1;
 
@@ -40,11 +41,65 @@ var PM = (function () {
 
         },
 
-        //
-        setDeltas : function () {
+        // set data values
+        setData : function () {
 
-            this.del = [];
+            var i = 0;
 
+            this.data = [];
+            while (i < this.pointCount) {
+
+                this.data.push({
+
+                    radius : 100,
+                    radian : Math.PI * 2 / this.pointCount * i,
+                    deltaRadius : Math.floor(Math.random() * 3) - 2,
+                    radiusMin : 20,
+                    radiusMax : 30
+
+                });
+
+                i += 1;
+
+            }
+
+        },
+
+        update : function () {
+
+            /*
+            var i = 0,
+            x,
+            y,
+            dx,
+            dy,
+            ra;
+
+            while (i < this.pointCount) {
+
+
+            ra = Math.PI * 2 / this.pointCount * i,
+
+            x = this.points[i * 2];
+            y = this.points[i * 2 + 1];
+            dx = x + Math.cos(ra) * this.del[i].radius;
+            dy = y + Math.sin(ra) * this.del[i].radius;
+
+            x += dx;
+            y += dy;
+
+            i += 1;
+
+
+
+            }
+
+            // splice out the last point
+            // and set it to the first point
+            this.points.splice(this.points.length - 2, 1, this.points[0]);
+            this.points.splice(this.points.length - 1, 1, this.points[1]);
+
+             */
         }
 
     };
@@ -71,6 +126,8 @@ var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
             var gra = game.world.children[0];
 
             gra.clear();
+
+            PM.update();
 
             gra.lineStyle(3, 0x00ff00);
 
