d08b5c5a8da680827c1a5a0fbec57217ab2eda2c
dustinpfister <dustin.pfister@gmail.com>
2017-10-24 13:26:56 -0400

    new demo

--- public/demos/component_input_enabled/main.js
+++ public/demos/component_input_enabled/main.js
@@ -1,4 +1,44 @@
 
+
+var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
+
+        create : function () {
+
+            var gra = game.add.graphics(160, 120),
+
+            draw = function (gra, color) {
+
+                color = color || 0x0000ff;
+
+                gra.clear();
+                gra.beginFill(color);
+                gra.drawRect(-50, -50, 100, 100);
+                gra.endFill();
+
+            };
+
+            // enable the input
+            gra.inputEnabled = true;
+
+            // I can now use the onInputDown event
+            gra.events.onInputDown.add(function (gra, pt) {
+
+                draw(gra, 0x00ff00);
+
+            });
+
+            // onInputUp also
+            gra.events.onInputUp.add(function (gra, pt) {
+
+                draw(gra, 0x0000ff);
+
+            });
+
+            draw(gra);
+        }
+
+    });
+
 /*
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { create: create });
 
@@ -62,261 +102,219 @@ drawShape(0x027a71, 0x02fdeb);
 
 }
  */
-
+/*
 // Poly Model Example
 var PM = (function () {
 
-    var api = {
+var api = {
 
-        pointCount : 10,
+pointCount : 10,
 
-        // the points
-        points : [],
+// the points
+points : [],
 
-        // parallel array of delta values for the points
-        data : [],
+// parallel array of delta values for the points
+data : [],
 
-        // set the staring status of points
-        setPoints : function () {
+// set the staring status of points
+setPoints : function () {
 
-            // current point index
-            var pi = 0,
-            data;
+// current point index
+var pi = 0,
+data;
 
-            this.points = [];
+this.points = [];
 
-            // set the values for each point
-            this.setData();
-            while (pi < this.pointCount) {
+// set the values for each point
+this.setData();
+while (pi < this.pointCount) {
 
-                data = this.data[pi];
+data = this.data[pi];
 
-                // push x first, then y
-                this.points.push(Math.floor(Math.cos(data.radian) * data.radius));
-                this.points.push(Math.floor(y = Math.sin(data.radian) * data.radius));
+// push x first, then y
+this.points.push(Math.floor(Math.cos(data.radian) * data.radius));
+this.points.push(Math.floor(y = Math.sin(data.radian) * data.radius));
 
-                pi += 1;
-
-            }
-
-            // push first point at the end
-            this.points.push(this.points[0]);
-            this.points.push(this.points[1]);
-
-        },
-
-        forAll : function (func) {
-
-            var i = 0,
-            api;
-            while (i < this.pointCount) {
-
-                api = {
-
-                    i : i,
-                    xi : i * 2,
-                    yi : i * 2 + 1,
-                    data : this.data[i],
-                    points : this.points,
-                    pointCount : this.pointCount
-
-                };
-
-                api.x = api.points[api.xi];
-                api.y = api.points[api.yi];
-
-                func.call(api, api.x, api.y);
-
-                i += 1;
-            }
-
-        },
-
-        // set data values
-        setData : function () {
-
-            var i = 0;
-
-            this.data = [];
-            while (i < this.pointCount) {
-
-                this.data.push({
-
-                    radius : 80,
-                    radian : Math.PI * 2 / this.pointCount * i,
-                    deltaRadius : 1 - Math.floor(Math.random() * 2) * 2,
-                    radiusMin : 70 - Math.floor(Math.random() * 30),
-                    radiusMax : 90 + Math.floor(Math.random() * 30),
-                    rate : 33 + Math.floor(66 * Math.random()),
-                    lastTime : new Date(),
-                    prop : Math.random() * .25 + .25
-
-                });
-
-                i += 1;
-
-            }
-
-        },
-
-        // user action
-        userAction : function (x, y) {
-
-            console.log(x + ',' + y);
-
-            this.forAll(function (ptx, pty) {
-
-                // Math.sqrt( Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2) )
-
-                var d = Math.sqrt(Math.pow(x - ptx, 2) + Math.pow(y - pty, 2));
-
-                var per = 1 - d / 150;
-
-                per = per > 1 ? 1 : per;
-                per = per < 0 ? 0 : per;
-
-                this.data.radius = this.data.radiusMin + (this.data.radiusMax - this.data.radiusMin) * per;
-            });
-
-            this.update();
-
-        },
-
-        update : function () {
-
-            var i = 0,
-            roll,
-            now,
-            data;
-            while (i < this.pointCount) {
-
-                data = this.data[i];
-
-                this.points[i * 2] = Math.cos(data.radian) * data.radius;
-                this.points[i * 2 + 1] = Math.sin(data.radian) * data.radius;
-
-                /*
-                now = new Date();
-                roll = Math.random();
-
-                if (roll < data.prop) {
-
-                data.rate = 33 + Math.floor(66 * Math.random())
-
-                }
-                 *
-                if (now - data.lastTime >= data.rate) {
-
-                data.radius += data.deltaRadius;
-
-                if (data.radius <= data.radiusMin) {
-
-                data.radius = data.radiusMin;
-                data.deltaRadius = 1;
-
-                }
-
-                if (data.radius >= data.radiusMax) {
-
-                data.radius = data.radiusMax;
-                data.deltaRadius = -1;
-
-                }
-
-                this.points[i * 2] = Math.cos(data.radian) * data.radius;
-                this.points[i * 2 + 1] = Math.sin(data.radian) * data.radius;
-
-                data.lastTime = now;
-                }
-
-                 */
-                i += 1;
-
-            }
-
-            // splice out the last point
-            // and set it to the first point
-            this.points.splice(this.points.length - 2, 1, this.points[0]);
-            this.points.splice(this.points.length - 1, 1, this.points[1]);
-
-        }
-
-    };
-
-    return api;
+pi += 1;
 
 }
-    ());
+
+// push first point at the end
+this.points.push(this.points[0]);
+this.points.push(this.points[1]);
+
+},
+
+forAll : function (func) {
+
+var i = 0,
+api;
+while (i < this.pointCount) {
+
+api = {
+
+i : i,
+xi : i * 2,
+yi : i * 2 + 1,
+data : this.data[i],
+points : this.points,
+pointCount : this.pointCount
+
+};
+
+api.x = api.points[api.xi];
+api.y = api.points[api.yi];
+
+func.call(api, api.x, api.y);
+
+i += 1;
+}
+
+},
+
+// set data values
+setData : function () {
+
+var i = 0;
+
+this.data = [];
+while (i < this.pointCount) {
+
+this.data.push({
+
+radius : 80,
+radian : Math.PI * 2 / this.pointCount * i,
+deltaRadius : 1 - Math.floor(Math.random() * 2) * 2,
+radiusMin : 70 - Math.floor(Math.random() * 30),
+radiusMax : 90 + Math.floor(Math.random() * 30),
+rate : 33 + Math.floor(66 * Math.random()),
+lastTime : new Date(),
+prop : Math.random() * .25 + .25
+
+});
+
+i += 1;
+
+}
+
+},
+
+// user action
+userAction : function (x, y) {
+
+console.log(x + ',' + y);
+
+this.forAll(function (ptx, pty) {
+
+// Math.sqrt( Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2) )
+
+var d = Math.sqrt(Math.pow(x - ptx, 2) + Math.pow(y - pty, 2));
+
+var per = 1 - d / 150;
+
+per = per > 1 ? 1 : per;
+per = per < 0 ? 0 : per;
+
+this.data.radius = this.data.radiusMin + (this.data.radiusMax - this.data.radiusMin) * per;
+});
+
+this.update();
+
+},
+
+update : function () {
+
+var i = 0,
+roll,
+now,
+data;
+while (i < this.pointCount) {
+
+data = this.data[i];
+
+this.points[i * 2] = Math.cos(data.radian) * data.radius;
+this.points[i * 2 + 1] = Math.sin(data.radian) * data.radius;
+
+i += 1;
+
+}
+
+// splice out the last point
+// and set it to the first point
+this.points.splice(this.points.length - 2, 1, this.points[0]);
+this.points.splice(this.points.length - 1, 1, this.points[1]);
+
+}
+
+};
+
+return api;
+
+}
+());
 
 var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
 
-        // create method
-        create : function () {
+// create method
+create : function () {
 
-            PM.setPoints();
+var back = game.add.graphics(game.world.centerX, game.world.centerY),
+gra,
+draw;
 
-            var back = game.add.graphics(game.world.centerX, game.world.centerY);
+PM.setPoints();
 
-            back.lineStyle(3, 000000, .4);
+back.lineStyle(3, 000000, .4);
 
-            PM.forAll(function (x, y) {
+PM.forAll(function (x, y) {
 
-                back.moveTo(Math.cos(this.data.radian) * this.data.radiusMin,
-                    Math.sin(this.data.radian) * this.data.radiusMin);
-                back.lineTo(
+back.moveTo(Math.cos(this.data.radian) * this.data.radiusMin,
+Math.sin(this.data.radian) * this.data.radiusMin);
+back.lineTo(
 
-                    Math.cos(this.data.radian) * this.data.radiusMax,
-                    Math.sin(this.data.radian) * this.data.radiusMax);
+Math.cos(this.data.radian) * this.data.radiusMax,
+Math.sin(this.data.radian) * this.data.radiusMax);
 
-            });
+});
 
-            // add a graphics object to the world
-            var gra = game.add.graphics(game.world.centerX, game.world.centerY);
+// add a graphics object to the world
+gra = game.add.graphics(game.world.centerX, game.world.centerY);
 
-            gra.inputEnabled = true;
-            //gra.input.useHandCursor = true;
-			//gra.input.draggable = true;
+draw = function () {
 
-			//console.log(gra.input.useHandCursor);
-			
-            var draw = function () {
+gra.clear();
 
-                gra.clear();
+gra.lineStyle(6, 0x000000);
 
-                gra.lineStyle(6, 0x000000);
+gra.beginFill(0xff0000);
+gra.alpha = .4;
+gra.drawPolygon(PM.points);
 
-                gra.beginFill(0xffffff);
-                gra.alpha = .4;
-                gra.drawPolygon(PM.points);
+gra.lineStyle(2, 0xffffff);
 
-                gra.lineStyle(2, 0xffffff);
+gra.drawPolygon(PM.points);
+gra.endFill();
 
-                gra.drawPolygon(PM.points);
-                gra.endFill();
+}
 
-            }
+// enable input for the sprite gra
+gra.inputEnabled = true;
 
-			console.log(gra.events);
-			
-            gra.events.onInputDown.add(function (gra, pt) {
+// use the hand cursor it is is a mouse
+gra.input.useHandCursor = true;
 
-                PM.userAction(pt.x - gra.x, pt.y - gra.y);
-                //PM.update();
-                draw();
+gra.events.onInputDown.add(function (gra, pt) {
 
-            })
-			
-			
-			/*
-            gra.events.onDragUpdate.add(function (gra, pt) {
+PM.userAction(pt.x - gra.x, pt.y - gra.y);
+//PM.update();
+draw();
 
-			console.log('okay');
+})
 
-            })
-			*/
+draw();
 
-            draw();
+}
 
-        }
+}, true);
 
-    }, true);
+*/
