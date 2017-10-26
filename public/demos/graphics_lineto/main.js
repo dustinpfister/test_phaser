fdbd4d5c613d7c6b5f65a6bbdd2ae59ba634cba7
dustinpfister <dustin.pfister@gmail.com>
2017-10-26 18:08:32 -0400

    okay I like my line demo

--- public/demos/graphics_lineto/main.js
+++ public/demos/graphics_lineto/main.js
@@ -7,38 +7,82 @@ var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
             // add a graphics object to the world
             var gra = game.add.graphics(0, 0);
 
-            // make it a red rectangle
-            gra.lineStyle(3, 0xff0000);
-
+            // making an interesting data object for this one
             gra.data = {
 
-                maxLines : 10,
+                maxLines : 20,
+                maxSets : 4,
                 lines : [],
                 sx : 0,
                 sy : game.world.height / 2,
+
+                drawCount : 2,
+                lastTime : new Date(),
+                rate : 150,
+
                 genLines : function () {
 
-                    var i = 0,
-                    sx = this.sx,
-                    sy = this.sy,
+                    var i,
+                    sx,
+                    sy,
                     ex,
                     ey,
-                    line;
+                    line,
+                    h = game.world.height,
+                    hh = h / 2,
+                    range = h / 2,
+                    setI = 0;
 
-                    // gen lines
-                    while (i < this.maxLines) {
+                    this.lines = [];
 
-                        ex = game.world.width / this.maxLines * (i+1);
-                        ey = Math.random() * game.world.height;
+                    // make line sets
+                    while (setI < this.maxSets) {
 
-                        line = new Phaser.Line(sx, sy, ex, ey);
+                        i = 0;
+                        sx = this.sx;
+                        sy = this.sy;
 
-                        this.lines.push(line);
+                        // gen lines for current set
+                        while (i < this.maxLines) {
 
-                        sx = ex;
-                        sy = ey;
+                            range = hh * (i / this.maxLines);
 
-                        i += 1;
+                            ex = game.world.width / this.maxLines * (i + 1);
+                            ey = Math.random() * range + (hh - range / 2);
+
+                            line = new Phaser.Line(sx, sy, ex, ey);
+
+                            this.lines.push(line);
+
+                            sx = ex;
+                            sy = ey;
+
+                            i += 1;
+                        }
+
+                        setI += 1;
+
+                    }
+
+                },
+
+                update : function () {
+
+                    var now = new Date();
+
+                    if (now - this.lastTime >= this.rate) {
+
+                        this.drawCount += 1;
+
+                        if (this.drawCount >= this.lines.length) {
+
+                            this.drawCount = 1;
+
+                            this.genLines();
+
+                        }
+
+                        this.lastTime = now;
                     }
 
                 },
@@ -46,13 +90,24 @@ var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
                 // draw to the given graphics object
                 draw : function (gra) {
 
+                    var i = 0,
+                    len = this.drawCount,
+                    //setI,
+                    per,
+                    line;
+
                     gra.clear();
 
-                    gra.lineStyle(3, 0xff0000);
+                    gra.lineStyle(5, 0x000000);
+                    while (i < len) {
 
-                    this.lines.forEach(function (line) {
+                        line = this.lines[i];
 
-                        console.log(line.start.y)
+                        //setI = Math.floor(i / this.maxLines);
+
+                        per = i / (len - 1);
+
+                        gra.lineAlpha = 1 - per;
 
                         // start by moving to a point
                         gra.moveTo(line.start.x, line.start.y);
@@ -60,7 +115,8 @@ var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
                         // draw a line
                         gra.lineTo(line.end.x, line.end.y);
 
-                    });
+                        i += 1;
+                    }
 
                 }
 
@@ -70,12 +126,18 @@ var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
 
             gra.data.draw(gra);
 
-
         },
 
-        update : function () {}
+        update : function () {
 
-    },true);
+            var gra = game.world.children[0];
+
+            gra.data.update();
+            gra.data.draw(gra);
+
+        }
+
+    }, true);
 
 /*
 var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
