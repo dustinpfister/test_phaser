481be6f8a91d4aa37c4e94dd12f2004e8291dfa6
dustinpfister <dustin.pfister@gmail.com>
2017-10-28 10:42:23 -0400

    update

--- public/demos/line/main.js
+++ public/demos/line/main.js
@@ -4,21 +4,21 @@ var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
         // create method
         create : function () {
 
+            // making a graphics display object
             var gra = game.add.graphics(0, 0),
 
             // making an instance of Phaser.Line
             line = new Phaser.Line(50, 190, 270, 190);
 
-            //
+            // draw the line
             gra.lineStyle(3, 0xff0000);
             gra.moveTo(line.start.x, line.start.y);
             gra.lineTo(line.end.x, line.end.y);
 
-            console.log(line);
+			
+            //console.log(line.midPoint());
 
-        },
-
-        // the update method will be called on each tick
-        update : function () {}
+			
+        }
 
     });
