40abf284d8bf8d3231442e010c54731c45a2fa19
dustinpfister <dustin.pfister@gmail.com>
2017-10-28 16:59:17 -0400

    update

--- public/demos/line/main.js
+++ public/demos/line/main.js
@@ -1,4 +1,7 @@
 
+console.log(new Phaser.Line());
+console.log(Phaser.Math);
+
 var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
 
         // create method
@@ -8,17 +11,71 @@ var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
             var gra = game.add.graphics(0, 0),
 
             // making an instance of Phaser.Line
-            line = new Phaser.Line(50, 190, 270, 190);
+            line1 = new Phaser.Line(-100, 0, 0, 0),
+            line2 = new Phaser.Line(0, 100, 100, 0),
 
-            // draw the line
+            // draw line method
+            drawLine = function (gra, line) {
+
+                gra.moveTo(line.start.x, line.start.y);
+                gra.lineTo(line.end.x, line.end.y);
+
+            };
+
+			line1.centerOn(100,120);
+			
+			console.log(line1.start.x); // 50
+			
+            // draw the lines
             gra.lineStyle(3, 0xff0000);
-            gra.moveTo(line.start.x, line.start.y);
-            gra.lineTo(line.end.x, line.end.y);
+            drawLine(gra, line1);
+            drawLine(gra, line2);
 
-			
-            //console.log(line.midPoint());
+            var cp = Phaser.Line.intersects(line1, line2);
 
+			if(cp){
+            
+			   console.log(cp.x + ',' + cp.y);
 			
+			}
+			console.log(line1.midPoint())
+
         }
 
     });
+
+/*
+var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', {
+
+// create method
+create : function () {
+
+// making a graphics display object
+var gra = game.add.graphics(0, 0),
+
+// making an instance of Phaser.Line
+baseLine = new Phaser.Line(50, 190, 270, 190),
+
+// get the midpoint of the baseline
+mp = baseLine.midPoint(),
+
+// make a new center line off from the midpoint of baseLine
+centerLine = new Phaser.Line(mp.x, mp.y, mp.x, baseLine.y - 100),
+
+// draw line method
+drawLine = function (gra, line) {
+
+gra.moveTo(line.start.x, line.start.y);
+gra.lineTo(line.end.x, line.end.y);
+
+}
+
+// draw the lines
+gra.lineStyle(3, 0xff0000);
+drawLine(gra, baseLine);
+drawLine(gra, centerLine);
+
+}
+
+});
+*/
