e77e2d0ec32d4a7c1a7ee0babef0acefcf1426af
dustinpfister <dustin.pfister@gmail.com>
2018-08-04 14:30:12 -0400

    state selector

---
+++ public/forpost/phaser-sprite-from-canvas/boot.js
@@ -0,0 +1,9 @@
+
+// start out on basic
+game.state.start('basic');
+
+$('#state-select').on('change', function (evnt) {
+
+    game.state.start(evnt.target.value);
+
+})
--- public/forpost/phaser-sprite-from-canvas/main.js
+++ public/forpost/phaser-sprite-from-canvas/main.js
@@ -1,3 +1,24 @@
+var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
+
+game.global = {
+
+    per: 0,
+    r: 0,
+    bias: 0,
+    frame: 0,
+    maxFrame: 150,
+    step: function () {
+
+        this.frame += 1;
+        this.frame %= this.maxFrame;
+
+        this.per = this.frame / this.maxFrame;
+        this.r = Math.PI * 2 * this.per;
+        this.bias = Math.abs(this.per - 0.5) / 0.5;
+
+    }
+
+};
 
 var fromCanvas = function (opt) {
 
@@ -55,130 +76,3 @@ var fromCanvas = function (opt) {
     return sprite
 
 };
-
-var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
-
-game.global = {
-
-    per: 0,
-    r: 0,
-    bias: 0,
-    frame: 0,
-    maxFrame: 150,
-    step: function () {
-
-        this.frame += 1;
-        this.frame %= this.maxFrame;
-
-        this.per = this.frame / this.maxFrame;
-        this.r = Math.PI * 2 * this.per;
-        this.bias = Math.abs(this.per - 0.5) / 0.5;
-
-    }
-
-};
-
-// full demo state
-game.state.add('demo', {
-
-    create: function () {
-
-        var sp3,
-        canvas,
-        ctx;
-
-        // creates a new canvas, and uses a render method for it
-        fromCanvas({
-            game: game,
-            name: 'sp1',
-            width: 32,
-            height: 128,
-            render: function (ctx) {
-                ctx.strokeStyle = 'green';
-                ctx.lineWidth = 6;
-                ctx.strokeRect(0, 0, this.width, this.height);
-            }
-        });
-
-        // this represents a canvas that was canvas before hand
-        canvas = document.createElement('canvas'),
-        ctx = canvas.getContext('2d');
-        canvas.width = 32;
-        canvas.height = 32;
-        ctx.lineWidth = 3;
-        ctx.strokeStyle = 'white';
-        ctx.fillStyle = 'red';
-        ctx.beginPath();
-        ctx.arc(16, 16, 14, 0, Math.PI * 2);
-        ctx.closePath();
-        ctx.fill();
-        ctx.stroke();
-
-        // uses a canvas that was created before hand
-        fromCanvas({
-            game: game,
-            name: 'sp2',
-            cacheBitmap: true, // cache the bitmap data instance
-            canvas: canvas
-        });
-
-        // I can make more sprites with the cached bitmap from sp2
-        sp3 = game.add.sprite(0, 0, game.cache.getBitmapData('bitmap-sp2'));
-        sp3.name = 'sp3';
-
-    },
-
-    update: function () {
-
-        var sp1 = game.world.getByName('sp1'),
-        sp2 = game.world.getByName('sp2'),
-        sp3 = game.world.getByName('sp3');
-
-        sp1.x = game.global.bias * (game.world.width - sp1.width);
-        sp1.y = game.world.centerY - sp1.height / 2;
-
-        sp2.x = Math.cos(game.global.r) * 75 + game.world.centerX - sp2.width / 2;
-        sp2.y = Math.sin(game.global.r) * 75 + game.world.centerY - sp2.height / 2;
-
-        sp3.x = game.world.centerX - sp3.width / 2;
-        sp3.y = game.world.centerY - sp3.height / 2;
-
-        game.global.step();
-
-    }
-
-});
-
-// basic state
-game.state.add('basic', {
-
-    create: function () {
-
-        // create a canvas, and get the 2d context
-        var canvas = document.createElement('canvas'),
-        ctx = canvas.getContext('2d'),
-        bitmap,
-        sprite;
-
-        // set the native width, and height of the canvas
-        // and draw something to the context
-        canvas.width = 64;
-        canvas.height = 64;
-        ctx.fillStyle = 'green';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-        // make a bitmap, and draw the canvas to its canvas
-        bitmap = new Phaser.BitmapData(game, '', 64, 64);
-        bitmap.context.drawImage(canvas, 0, 0);
-
-        // use that bitmap as the texture for the sprite
-        sprite = game.add.sprite(0, 0, bitmap);
-        sprite.name = 'bx';
-        sprite.x = game.world.centerX - sprite.width / 2;
-        sprite.y = game.world.centerY - sprite.height / 2;
-
-    }
-
-});
-
-game.state.start('basic');
