8fe6ecfc99176da5e0c58ad5d4971b802bb8df65
dustinpfister <dustin.pfister@gmail.com>
2018-08-04 14:37:15 -0400

    bitmap only

---
+++ public/forpost/phaser-sprite-from-canvas/bitmaponly.js
@@ -0,0 +1,24 @@
+// basic state
+game.state.add('bitmaponly', {
+
+    create: function () {
+
+        var bitmap,
+        ctx,
+        sprite;
+
+        // make a bitmap, and draw to the canvas context
+        bitmap = new Phaser.BitmapData(game, '', 64, 64);
+        ctx = bitmap.context;
+        ctx.fillStyle = 'blue';
+        ctx.fillRect(0, 0, bitmap.width, bitmap.height);
+
+        // use that bitmap as the texture for the sprite
+        sprite = game.add.sprite(0, 0, bitmap);
+        sprite.name = 'bx';
+        sprite.x = game.world.centerX - sprite.width / 2;
+        sprite.y = game.world.centerY - sprite.height / 2;
+
+    }
+
+});
