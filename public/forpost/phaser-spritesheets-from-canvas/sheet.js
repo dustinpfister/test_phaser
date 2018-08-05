200bd83bcfbe7a6ec48b32eeda043afff3c438df
dustinpfister <dustin.pfister@gmail.com>
2018-08-04 20:16:03 -0400

    basic demo is working okay

--- public/forpost/phaser-spritesheets-from-canvas/sheet.js
+++ public/forpost/phaser-spritesheets-from-canvas/sheet.js
@@ -1,37 +1,68 @@
-game.state.add('sheet', {
+game.state.add('basic', {
+
+    data: {
+        maxFrame: 24,
+        frameRate: 33,
+        lastFrame: new Date()
+    },
 
     create: function () {
 
         var frame = 0,
-        maxFrame = 3,
-        bitmap = new Phaser.BitmapData(game, '', 32 * maxFrame, 32),
-        ctx;
+        maxFrame = this.data.maxFrame,
+        canvas = document.createElement('canvas');
+        ctx = canvas.getContext('2d');
 
-        ctx = bitmap.context;
+        canvas.width = 32 * maxFrame;
+        canvas.height = 32;
         while (frame < maxFrame) {
 
             var sx = 32 * frame + 0.5,
             per = frame / maxFrame;
 
             ctx.strokeStyle = '#00ff00';
-            ctx.lineWidth = 1;
-            ctx.strokeRect(sx + 8 * per, 0, 16 - 8 * per, 16);
+
+            ctx.save();
+            ctx.translate(sx + 16, 16);
+            ctx.rotate(Math.PI * 2 * per);
+            ctx.strokeRect(-8, -8, 16, 16);
+            ctx.beginPath();
+            ctx.moveTo(0, 0);
+            ctx.lineTo(0, 15);
+            ctx.stroke();
+            ctx.restore();
 
             frame += 1;
 
         }
 
-        game.cache.addSpriteSheet('sheet-1', null, bitmap.canvas, 32, 32, 3, 0, 0);
+        game.cache.addSpriteSheet('sheet-1', null, canvas, 32, 32, maxFrame, 0, 0);
 
-        console.log(game.cache);
-
-        var sprite = game.add.sprite(0, 0, 'sheet-1', 2);
+        var sprite = game.add.sprite(0, 0, 'sheet-1', 0);
         sprite.smoothed = false;
+        sprite.name = 'sp1';
+
+    },
+
+    update: function () {
+
+        var sprite = game.world.getByName('sp1'),
+        now = new Date();
+
         sprite.x = 32;
         sprite.y = 32;
+
+        if (now - this.data.lastFrame >= this.data.frameRate) {
+
+            sprite.frame += 1;
+            sprite.frame %= this.data.maxFrame;
+
+            this.data.lastFrame = new Date();
+
+        }
 
     }
 
 });
 
-game.state.start('sheet');
+game.state.start('basic');
