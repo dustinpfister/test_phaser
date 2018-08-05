7aa0eb07bd15861b7fb88e71614794d0ee8698b3
dustinpfister <dustin.pfister@gmail.com>
2018-08-04 20:23:02 -0400

    cleanup

--- public/forpost/phaser-spritesheets-from-canvas/basic.js
+++ public/forpost/phaser-spritesheets-from-canvas/basic.js
@@ -2,7 +2,7 @@ game.state.add('basic', {
 
     data: {
         maxFrame: 24,
-        frameRate: 33,
+        frameRate: 100,
         lastFrame: new Date()
     },
 
@@ -10,47 +10,53 @@ game.state.add('basic', {
 
         var frame = 0,
         maxFrame = this.data.maxFrame,
+        frameWidth = 64,
+        frameHeight = 64,
         canvas = document.createElement('canvas');
         ctx = canvas.getContext('2d');
 
-        canvas.width = 32 * maxFrame;
-        canvas.height = 32;
+        canvas.width = frameWidth * maxFrame;
+        canvas.height = frameHeight;
         while (frame < maxFrame) {
 
-            var sx = 32 * frame + 0.5,
+            // figure startx, and percent done
+            var sx = frameWidth * frame + 0.5,
             per = frame / maxFrame;
 
+            // draw for current frame
             ctx.strokeStyle = '#00ff00';
-
             ctx.save();
-            ctx.translate(sx + 16, 16);
+            ctx.translate(sx + 32, 32);
             ctx.rotate(Math.PI * 2 * per);
-            ctx.strokeRect(-8, -8, 16, 16);
+            ctx.strokeRect(-16, -16, 32, 32);
             ctx.beginPath();
             ctx.moveTo(0, 0);
-            ctx.lineTo(0, 15);
+            ctx.lineTo(0, 30);
             ctx.stroke();
             ctx.restore();
 
+            // next frame
             frame += 1;
 
         }
 
-        game.cache.addSpriteSheet('sheet-1', null, canvas, 32, 32, maxFrame, 0, 0);
+        // add a new sheet to cache
+        game.cache.addSpriteSheet('sheet-1', null, canvas, frameWidth, frameHeight, maxFrame, 0, 0);
 
+        // create a sprite with the sheet
         var sprite = game.add.sprite(0, 0, 'sheet-1', 0);
         sprite.smoothed = false;
         sprite.name = 'sp1';
+        sprite.x = game.world.centerX - sprite.width / 2;
+        sprite.y = game.world.centerY - sprite.height / 2;
 
     },
 
+    // loop frames
     update: function () {
 
         var sprite = game.world.getByName('sp1'),
         now = new Date();
-
-        sprite.x = 32;
-        sprite.y = 32;
 
         if (now - this.data.lastFrame >= this.data.frameRate) {
 
