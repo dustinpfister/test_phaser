b41affe580235181ce67a553ead7f4b48f97feac
dustinpfister <dustin.pfister@gmail.com>
2018-08-06 15:28:51 -0400

    objects

--- public/forpost/phaser-world-move-around/main.js
+++ public/forpost/phaser-world-move-around/main.js
@@ -10,7 +10,40 @@ game.state.add('world-demo', {
 
     create: function () {
 
+        var i,
+        len,
+        x,
+        y
+
         game.world.resize(640, 240);
+
+        sheetFromCanvas({
+
+            game: game,
+            name: 'sheet-object',
+            frames: 1,
+            forFrame: function (ctx) {
+
+                ctx.fillStyle = '#0000ff';
+                ctx.fillRect(0, 0, 32, 32);
+
+            }
+
+        });
+
+        i = 0;
+        len = 20;
+        while (i < len) {
+
+            x = Math.floor(Math.random() * (game.world.width - 32));
+            y = Math.floor(Math.random() * (game.world.height - 32));
+
+            sprite = game.add.sprite(x, y, 'sheet-object', 0);
+            sprite.name = 'object' + i;
+
+            i += 1;
+
+        }
 
         sheetFromCanvas({
 
@@ -28,8 +61,13 @@ game.state.add('world-demo', {
 
         var player = game.add.sprite(0, 0, 'sheet-player', 0);
         player.name = 'player';
+        player.data = {
+            dx: 0,
+            dy: 0
+        };
 
-        game.data.cursors = game.input.keyboard.createCursorKeys()
+        game.data.cursors = game.input.keyboard.createCursorKeys();
+        game.camera.target = player;
 
     },
 
@@ -40,27 +78,39 @@ game.state.add('world-demo', {
 
         if (cursors.right.isDown) {
 
-            player.x += 1;
+            player.data.dx += .25;
 
         }
 
         if (cursors.left.isDown) {
 
-            player.x -= 1;
+            player.data.dx -= .25;
 
         }
 
         if (cursors.down.isDown) {
 
-            player.y += 1;
+            player.data.dy += .25;
 
         }
 
         if (cursors.up.isDown) {
 
-            player.y -= 1;
+            player.data.dy -= .25;
 
         }
+
+        // clamp deltas
+        player.data.dx = Phaser.Math.clamp(player.data.dx, -5, 5);
+        player.data.dy = Phaser.Math.clamp(player.data.dy, -5, 5);
+
+        // add to player pos by deltas
+        player.x += player.data.dx;
+        player.y += player.data.dy;
+
+        // clamp player pos
+        player.x = Phaser.Math.clamp(player.x, 0, (game.world.width - 32));
+        player.y = Phaser.Math.clamp(player.y, 0, (game.world.height - 32));
 
     }
 
