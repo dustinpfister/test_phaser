594cfa8fbd6a115765da3a038f16bea3a4794995
dustinpfister <dustin.pfister@gmail.com>
2018-08-08 15:49:40 -0400

    another repeat example

--- public/forpost/phaser-time-events/repeat.js
+++ public/forpost/phaser-time-events/repeat.js
@@ -1,16 +1,41 @@
 
+/*
+game.state.add('repeat', {
+
+create: function () {
+
+var tick = 0;
+game.time.events.repeat(1000, 5, function () {
+
+console.log('tick: ' + tick);
+tick++;
+
+});
+
+}
+
+});
+ */
+
 game.state.add('repeat', {
 
     create: function () {
 
-        var tick = 0;
-        game.time.events.repeat(1000, 5, function () {
+        var tick = 0,
+        timer = game.time.events.loop(1000, function () {
 
             console.log('tick: ' + tick);
+
+            if (tick === 3) {
+
+                timer.loop = false;
+
+            }
+
             tick++;
 
         });
 
     }
 
-})
+});
