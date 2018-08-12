c8839db5e21279a3c28331a991f153b451a4fc6f
dustinpfister <dustin.pfister@gmail.com>
2018-08-11 20:27:22 -0400

    more advanced sheet from canvas

--- public/forpost/phaser-animations/main.js
+++ public/forpost/phaser-animations/main.js
@@ -9,22 +9,46 @@ game.state.add('ani-basic', {
     create: function () {
 
         sheetFromCanvas({
-
             game: game,
-            name: 'sheet-basic',
-            frames: 3,
-            forFrame: function (ctx) {
+            name: 'box-guy',
+            animations: [{
+                    frames: 2,
+                    forFrame: function (ctx) {
 
-                ctx.fillStyle = '#ff0000';
-                ctx.fillRect(0, 0, this.frameWidth, this.frameHeight)
+                        // red background
+                        ctx.fillStyle = '#ff0000';
+                        ctx.fillRect(0, 0, this.frameWidth, this.frameHeight)
 
-            }
+                    }
+                }, {
+                    frames: 3,
+                    forFrame: function (ctx) {
+
+                        console.log(this.sx);
+
+                        ctx.fillStyle = '#00ff00';
+                        ctx.fillRect(0, 0, this.frameWidth, this.frameHeight)
+
+                    }
+                }, {
+                    frames: 3,
+                    forFrame: function (ctx) {
+
+                        console.log(this.sx);
+
+                        ctx.fillStyle = '#0000ff';
+                        ctx.fillRect(0, 0, this.frameWidth, this.frameHeight)
+
+                    }
+                }
+            ]
+
         });
 
         game.add.sprite(0, 0, 'sheet-basic', 0);
 
     },
-    
+
     update: function () {}
 
 });
--- public/forpost/phaser-animations/sheet-from-canvas.js
+++ public/forpost/phaser-animations/sheet-from-canvas.js
@@ -1,43 +1,68 @@
 var sheetFromCanvas = function (opt) {
 
     var f,
+    sx,
     per,
     canvas = document.createElement('canvas');
     ctx = canvas.getContext('2d');
 
     opt = opt || {};
     opt.name = opt.name || '';
-    opt.frames = opt.frames || 3;
+    //opt.frames = opt.frames || 3;
     opt.frameWidth = opt.frameWidth || 32;
     opt.frameHeight = opt.frameHeight || 32;
-    opt.forFrame = opt.forFrame || function () {};
+    opt.animations = opt.animations || [{
+                frames: 1,
+                forFrame: function () {}
+            }
+        ];
     opt.game = opt.game || null;
 
-    canvas.width = opt.frameWidth * opt.frames;
+    var width = 0;
+    opt.animations.forEach(function (ani) {
+
+        width += ani.frames * opt.frameWidth;
+
+    });
+
+    canvas.width = width;
     canvas.height = opt.frameHeight;
 
-    f = 0;
-    while (f < opt.frames) {
-        per = f / opt.frames;
-        opt.forFrame.call({
-            f: f,
-            sx: opt.frameWidth * f + 0.5,
-            per: per,
-            canvas: canvas,
-            ctx: ctx,
-            frameWidth: opt.frameWidth,
-            frameHeight: opt.frameHeight
-        }, ctx);
-        f += 1;
-    }
+    var sx = 0;
+    opt.animations.forEach(function (ani) {
+
+        f = 0;
+        while (f < ani.frames) {
+
+            per = f / ani.frames;
+            ctx.save();
+            ctx.translate(sx + 0.5, 0);
+            ani.forFrame.call({
+                f: f,
+                sx: sx,
+                per: per,
+                bias: Math.abs(.5 - per) / .5,
+                canvas: canvas,
+                ctx: ctx,
+                frameWidth: opt.frameWidth,
+                frameHeight: opt.frameHeight
+            }, ctx);
+            ctx.restore();
+
+            sx += opt.frameWidth;
+            f += 1;
+        }
+
+    });
 
     // add a new sheet to cache if we have a game
     if (opt.game) {
         opt.game.cache.addSpriteSheet(opt.name, null, canvas, opt.frameWidth, opt.frameHeight, opt.frames, 0, 0);
     }
 
+    document.body.appendChild(canvas);
+
     // return the canvas
     return canvas;
 
 };
-
