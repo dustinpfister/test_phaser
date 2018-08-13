a8c0a2b4c0492a6b7d8ae8519d59554d7eed6a41
dustinpfister <dustin.pfister@gmail.com>
2018-08-13 11:07:00 -0400

    new demo on the phaser scale manager

---
+++ public/demos/scale_resize/main.js
@@ -0,0 +1,47 @@
+
+var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
+
+game.state.add('resize', {
+
+    create: function () {
+
+        // just a circle
+        var bx = game.add.graphics(160, 120);
+        bx.beginFill(0x00ff00);
+        bx.drawCircle(0, 0, 240);
+        bx.endFill();
+        game.stage.backgroundColor = '#2a2a2a';
+
+        game.input.onDown.add(function () {
+
+            if (game.scale.scaleMode === Phaser.ScaleManager.NO_SCALE) {
+
+                // set window constraints to 'visual' for both right, and bottom
+                game.scale.windowConstraints.right = 'visual';
+                game.scale.windowConstraints.bottom = 'visual';
+
+                // set scale mode to 'SHOW_ALL'
+                game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+            } else {
+
+                // set window constraints back to default
+                game.scale.windowConstraints.right = 'layout';
+                game.scale.windowConstraints.bottom = '';
+
+                // set scale back to NO_SCALE
+                game.scale.scaleMode = Phaser.ScaleManager.NO_SCALE;
+
+            }
+
+            console.log('window constraints: ' + JSON.stringify(game.scale.windowConstraints));
+            console.log('scale mode: ' + game.scale.scaleMode);
+            console.log('bounds: ' + game.scale.bounds);
+
+        });
+
+    }
+
+});
+
+game.state.start('resize');
