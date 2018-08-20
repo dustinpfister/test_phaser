f1dca5988e27b2e150af62d1d5e776e59f2c7de5
dustinpfister <dustin.pfister@gmail.com>
2018-08-20 11:12:41 -0400

    proto example

--- public/demos/point-rotate/main.js
+++ public/demos/point-rotate/main.js
@@ -24,4 +24,21 @@ game.state.add('static', {
 
 });
 
-game.state.start('static');
+game.state.add('proto', {
+
+    create: function () {
+
+        // objects for centerPoint, and a thing that
+        // I want to rotate around it.
+        var thing = new Phaser.Point(100, 100),
+        center = new Phaser.Point(0, 0);
+
+        thing.rotate(center.x, center.y, 45, true, 100);
+
+        console.log(Math.floor(thing.x), Math.floor(thing.y));
+
+    }
+
+});
+
+game.state.start('proto');
