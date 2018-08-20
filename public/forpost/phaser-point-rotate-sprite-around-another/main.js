824f4c72d9d7b75f163c465eb3549c403b87abf9
dustinpfister <dustin.pfister@gmail.com>
2018-08-20 16:43:35 -0400

    state switcher working

--- public/forpost/phaser-point-rotate-sprite-around-another/main.js
+++ public/forpost/phaser-point-rotate-sprite-around-another/main.js
@@ -1,48 +1,6 @@
 
 var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
 
-game.state.add('static', {
-
-    create: function () {
-
-        // objects for centerPoint, and a thing that
-        // I want to rotate around it.
-        var thing = {
-            x: 100,
-            y: 100
-        },
-        center = {
-            x: 0,
-            y: 0
-        };
-
-        // Using the static method
-        Phaser.Point.rotate(thing, center.x, center.y, 45, true, 100);
-
-        console.log(Math.floor(thing.x), Math.floor(thing.y)); // 0 100
-
-    }
-
-});
-
-game.state.add('proto', {
-
-    create: function () {
-
-        // objects for centerPoint, and a thing that
-        // I want to rotate around it.
-        var thing = new Phaser.Point(100, 100),
-        center = new Phaser.Point(0, 0);
-
-        // Using the prototype method
-        thing.rotate(center.x, center.y, 45, true, 100);
-
-        console.log(Math.floor(thing.x), Math.floor(thing.y)); // 0 100
-
-    }
-
-});
-
 // make a sprite sheet, and start sprites state
 game.state.add('boot', {
 
@@ -95,6 +53,7 @@ var mkSprites = function () {
 
 };
 
+// switch states
 var stateSwitcher = function () {
 
     game.input.onDown.add(function () {
@@ -226,3 +185,48 @@ game.state.add('sprites_3', {
 });
 
 game.state.start('boot');
+
+
+/*
+game.state.add('static', {
+
+    create: function () {
+
+        // objects for centerPoint, and a thing that
+        // I want to rotate around it.
+        var thing = {
+            x: 100,
+            y: 100
+        },
+        center = {
+            x: 0,
+            y: 0
+        };
+
+        // Using the static method
+        Phaser.Point.rotate(thing, center.x, center.y, 45, true, 100);
+
+        console.log(Math.floor(thing.x), Math.floor(thing.y)); // 0 100
+
+    }
+
+});
+
+game.state.add('proto', {
+
+    create: function () {
+
+        // objects for centerPoint, and a thing that
+        // I want to rotate around it.
+        var thing = new Phaser.Point(100, 100),
+        center = new Phaser.Point(0, 0);
+
+        // Using the prototype method
+        thing.rotate(center.x, center.y, 45, true, 100);
+
+        console.log(Math.floor(thing.x), Math.floor(thing.y)); // 0 100
+
+    }
+
+});
+*/
