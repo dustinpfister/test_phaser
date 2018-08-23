43968fb88f62a0d3af305a7e46c273ed7571dc48
dustinpfister <dustin.pfister@gmail.com>
2018-08-23 11:25:36 -0400

    create canvas state

--- public/forpost/phaser-point-setmagnitude/main.js
+++ public/forpost/phaser-point-setmagnitude/main.js
@@ -1,12 +1,6 @@
 var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
 
-game.state.add('example', {
-
-    create: function () {}
-
-});
-
-// boot state
+// boot state will be called first
 game.state.add('boot', {
 
     create: function () {
@@ -25,9 +19,44 @@ game.state.add('boot', {
         game.scale.height = document.getElementById(game.parent).scrollHeight;
 
         // start buttons state
+        game.state.start('gen-sheet');
+
+    }
+
+});
+
+// generate a sprite sheet using canvas
+game.state.add('gen-sheet', {
+
+    create: function () {
+
+        var canvas = document.createElement('canvas'),
+        ctx = canvas.getContext('2d');
+
+        canvas.width = 32;
+        canvas.height = 32;
+
+        ctx.fillStyle = 'white';
+        ctx.strokeStyle = 'black';
+
+        ctx.beginPath();
+        ctx.arc(16, 16, 14, 0, Math.PI * 2);
+        ctx.closePath();
+        ctx.stroke();
+        ctx.fill();
+
+        this.game.cache.addSpriteSheet('sheet-1', null, canvas, 32, 32, 1, 0, 0);
+
         game.state.start('example');
 
     }
+
+});
+
+// the example
+game.state.add('gen-sheet', {
+
+    create: function () {}
 
 });
 
