27537f3553506e96158e11b696f318963cf18543
dustinpfister <dustin.pfister@gmail.com>
2018-08-26 18:36:17 -0400

    adding text display objects to a group

--- public/forpost/phaser-groups-remove/main.js
+++ public/forpost/phaser-groups-remove/main.js
@@ -6,8 +6,21 @@ game.state.add('basics-1', {
 
     create: function () {
 
+        var group = game.add.group();
 
+        var i = 5;
+        while (i--) {
+
+            group.add(game.add.text(0, i * 20, 'foo', {
+                    fill: 'red'
+                }));
+
+        }
+
+        console.log(group.children.length); // 5
 
     }
 
 });
+
+game.state.start('basics-1');
