960e7ad4ce7cb598d110a2957bc1c5c6bcda55aa
dustinpfister <dustin.pfister@gmail.com>
2018-08-27 14:37:42 -0400

    release method

--- public/forpost/phaser-groups-nesting/main.js
+++ public/forpost/phaser-groups-nesting/main.js
@@ -1,5 +1,70 @@
 var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
 
+// round object
+var round = {
+
+    waves: {},
+    cache: {},
+    game: game,
+
+    // setup waves
+    setup: function () {
+
+        var waveCount = 3,
+        enemysPerWave = 5,
+        wave,
+        enemy,
+        wi,
+        ei;
+
+        this.waves = game.add.group();
+        this.waves.name = 'waves';
+        this.waves.x = -32;
+
+        this.cache = game.add.group();
+        this.cache.name = 'cache';
+        this.cache.x = -32;
+
+        // for each
+        wi = 0;
+        while (wi < waveCount) {
+
+            // add new wave as parent of this.waves
+            wave = game.add.group(this.waves);
+            wave.name = 'wave-' + (wi + 1);
+
+            ei = 0;
+            while (ei < enemysPerWave) {
+
+                enemy = wave.create(0, 0, 'badguys');
+                wave.name = 'wave-' + (wi + 1);
+
+                ei += 1;
+            }
+
+            wi += 1;
+
+        }
+
+    },
+
+    // release the next enemy from cache
+    release: function () {
+
+        if (this.cache.children.length > 0) {
+
+            var enemy = this.cache.children[0];
+
+            this.cache.remove(enemy, false);
+
+            this.game.add(enemy);
+
+        }
+
+    }
+
+};
+
 game.state.add('example-1', {
 
     create: function () {
@@ -13,49 +78,7 @@ game.state.add('example-1', {
         ctx.fillRect(0, 0, 32, 32);
         this.game.cache.addSpriteSheet('badguys', null, canvas, 32, 32, 1, 0, 0);
 
-        var round = {
-
-            waves: game.add.group(),
-            active: game.add.group(),
-
-            // setup waves
-            setup: function () {
-
-                var waveCount = 3,
-                enemysPerWave = 5,
-                wave,
-                enemy,
-                wi,
-                ei;
-
-                this.waves.name = 'waves';
-                this.active.name = 'active';
-
-                // for each
-                wi = 0;
-                while (wi < waveCount) {
-
-                    // add new wave as parent of this.waves
-                    wave = game.add.group(this.waves);
-                    wave.name = 'wave-' + (wi + 1);
-                    wave.x = -32;
-
-                    ei = 0;
-                    while (ei < enemysPerWave) {
-
-                        enemy = wave.create(0, 0, 'badguys');
-                        wave.name = 'wave-' + (wi + 1);
-
-                        ei += 1;
-                    }
-
-                    wi += 1;
-
-                }
-
-            }
-
-        };
+        // setup
         round.setup();
 
         console.log(round.waves.children);
@@ -64,37 +87,39 @@ game.state.add('example-1', {
 
 })
 
+game.state.start('example-1');
+
+/*
 game.state.add('game', {
 
-    create: function () {
+create: function () {
 
-        // green block sheet
-        sheetFromCanvas({
-            name: 'badguys',
-            game: this.game,
-            frames: 3,
-            frameWidth: 16,
-            frameHeight: 16,
-            forFrame: function (ctx) {
-                var colors = ['green', 'red', 'blue'];
-                ctx.fillStyle = colors[this.f];
-                ctx.lineWidth = 3;
-                ctx.fillRect(0, 0, 32, 32);
-            }
-        });
-
-        this.game.data = this.game.data || {};
-
-        var round = this.game.data.round = new Round({
-                game: this.game,
-                sheetKey: 'badguys',
-                waveCount: 10
-            });
-
-        game.time.events.loop(round.tick, 1000, round);
-
-    }
-
+// green block sheet
+sheetFromCanvas({
+name: 'badguys',
+game: this.game,
+frames: 3,
+frameWidth: 16,
+frameHeight: 16,
+forFrame: function (ctx) {
+var colors = ['green', 'red', 'blue'];
+ctx.fillStyle = colors[this.f];
+ctx.lineWidth = 3;
+ctx.fillRect(0, 0, 32, 32);
+}
 });
 
-game.state.start('example-1');
+this.game.data = this.game.data || {};
+
+var round = this.game.data.round = new Round({
+game: this.game,
+sheetKey: 'badguys',
+waveCount: 10
+});
+
+game.time.events.loop(round.tick, 1000, round);
+
+}
+
+});
+*/
