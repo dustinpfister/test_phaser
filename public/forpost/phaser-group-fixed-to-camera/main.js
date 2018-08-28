91d7368471898b58b34d0934480534a6c8fae1ad
dustinpfister <dustin.pfister@gmail.com>
2018-08-28 09:08:47 -0400

    demo is looking good

--- public/forpost/phaser-group-fixed-to-camera/main.js
+++ public/forpost/phaser-group-fixed-to-camera/main.js
@@ -9,6 +9,7 @@ game.state.add('example', {
 
         // MAKING THWE FIXED GROUP
         var fixed = game.add.group();
+        fixed.name = 'fixed';
         fixed.fixedToCamera = true;
         fixed.cameraOffset = new Phaser.Point(0, game.camera.height - 20);
 
@@ -17,7 +18,8 @@ game.state.add('example', {
             fill: 'white',
             font: '15px courier'
         };
-        var text = game.add.text(5, 5, 'fixed to camera', font);
+        var text = game.add.text(5, 5, '', font);
+        text.name = 'mess';
         fixed.add(text);
 
         // GENERATING SOME BOXES TO THE WORLD
@@ -41,9 +43,38 @@ game.state.add('example', {
 
         }
 
+        // SETTING SOME VALUES
+        // that will be used in the update method
+        var data = game.data = game.data || {};
+
+        data.frame = 0;
+        data.maxFrame = 200;
+        data.dist = game.world.height / 4;
+
     },
 
-    update: function () {}
+    update: function () {
+
+        var per = game.data.frame / game.data.maxFrame,
+        fixed = game.world.getByName('fixed'),
+        angle = Math.PI * 2 * per,
+        x,
+        y;
+
+        // updating position of the camera
+        x = Math.floor((game.world.width / 2) - (game.camera.width / 2) + Math.cos(angle) * game.data.dist);
+        y = Math.floor((game.world.height / 2) - (game.camera.height / 2) + Math.sin(angle) * game.data.dist);
+        game.camera.setPosition(x, y);
+
+        // displaying current camera position in text object
+        // that is in the fixed group
+        fixed.getByName('mess').text = 'fixed info: (' + x + ',' + y + ') ';
+
+        // step frame
+        game.data.frame += 1;
+        game.data.frame %= game.data.maxFrame;
+
+    }
 
 });
 
