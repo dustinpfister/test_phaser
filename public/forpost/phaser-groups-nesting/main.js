016846f2e896bef0c8902454b2038428af55a56b
dustinpfister <dustin.pfister@gmail.com>
2018-08-28 12:33:25 -0400

    round module is now broken up

--- public/forpost/phaser-groups-nesting/main.js
+++ public/forpost/phaser-groups-nesting/main.js
@@ -8,142 +8,142 @@ var round = {
 
     enemyCount: 0,
 
-    playerHP: 1000,
+    playerHP: 1000
 
-    // setup waves
-    setup: function (opt) {
+};
 
-        var waveCount = 3,
-        enemysPerWave = 5,
-        wave,
-        enemy,
-        wi,
-        ei;
+// setup waves
+round.setup =function (opt) {
 
-        opt = opt || {};
+    var waveCount = 3,
+    enemysPerWave = 5,
+    wave,
+    enemy,
+    wi,
+    ei;
 
-        this.game = opt.game || game;
+    opt = opt || {};
 
-        this.waves = game.add.group();
-        this.waves.name = 'waves';
-        this.waves.x = -32;
+    this.game = opt.game || game;
 
-        this.cache = game.add.group();
-        this.cache.name = 'cache';
-        this.cache.x = -32;
+    this.waves = game.add.group();
+    this.waves.name = 'waves';
+    this.waves.x = -32;
 
-        this.active = game.add.group();
-        this.active.name = 'active';
-        this.active.x = 0;
+    this.cache = game.add.group();
+    this.cache.name = 'cache';
+    this.cache.x = -32;
 
-        // 3 waves of 5
-        this.genWaves(3, 5);
+    this.active = game.add.group();
+    this.active.name = 'active';
+    this.active.x = 0;
 
-        // 1 wave of ten
-        this.genWaves(1, 10);
+    // 3 waves of 5
+    this.genWaves(3, 5);
 
-    },
+    // 1 wave of ten
+    this.genWaves(1, 10);
 
-    // generate the waves
-    genWaves: function (waveCount, enemysPerWave) {
+};
 
-        var wave,
-        enemy,
-        wi,
-        ei;
+// generate the waves
+round.genWaves = function (waveCount, enemysPerWave) {
 
-        // for each
-        wi = 0;
-        while (wi < waveCount) {
+    var wave,
+    enemy,
+    wi,
+    ei;
 
-            // add new wave as parent of this.waves (nesting)
-            wave = game.add.group(this.waves);
-            wave.name = 'wave-' + (wi + 1);
+    // for each
+    wi = 0;
+    while (wi < waveCount) {
 
-            ei = 0;
-            while (ei < enemysPerWave) {
+        // add new wave as parent of this.waves (nesting)
+        wave = game.add.group(this.waves);
+        wave.name = 'wave-' + (wi + 1);
 
-                enemy = wave.create(0, 0, 'badguys');
-                enemy.name = 'enemy-' + this.enemyCount;
-                enemy.inputEnabled = true;
+        ei = 0;
+        while (ei < enemysPerWave) {
 
-                // enemies die when clicked
-                enemy.events.onInputDown.add(function (enemy) {
+            enemy = wave.create(0, 0, 'badguys');
+            enemy.name = 'enemy-' + this.enemyCount;
+            enemy.inputEnabled = true;
 
-                    enemy.destroy();
-
-                });
-
-                this.enemyCount += 1;
-                ei += 1;
-            }
-
-            wi += 1;
-
-        }
-
-    },
-
-    // add next wave to cache
-    nextWave: function () {
-
-        if (this.waves.children.length > 0) {
-
-            var wave = this.waves.children[0],
-            enemy,
-            cache = this.cache,
-            i = wave.children.length;
-            while (i--) {
-                enemy = wave.children[i];
-                wave.remove(enemy);
-                cache.add(enemy);
-            }
-
-            wave.destroy();
-
-        }
-
-    },
-
-    // release the next enemy from cache
-    release: function () {
-
-        if (this.cache.children.length > 0) {
-
-            var enemy = this.cache.children[0];
-            enemy.x = Math.floor(Math.random() * (this.game.world.width - 32));
-            enemy.y = -32;
-
-            this.cache.remove(enemy, false);
-
-            this.active.add(enemy);
-
-        }
-
-    },
-
-    // tick the active group, where the real action happens
-    tick: function () {
-
-        var game = round.game;
-
-        this.active.forEach(function (enemy) {
-
-            enemy.y += 1;
-
-            if (enemy.y >= game.world.height) {
-
-                round.playerHP -= 5;
+            // enemies die when clicked
+            enemy.events.onInputDown.add(function (enemy) {
 
                 enemy.destroy();
 
-            }
+            });
 
-        });
+            this.enemyCount += 1;
+            ei += 1;
+        }
+
+        wi += 1;
 
     }
 
 };
+
+// add next wave to cache
+round.nextWave = function () {
+
+    if (this.waves.children.length > 0) {
+
+        var wave = this.waves.children[0],
+        enemy,
+        cache = this.cache,
+        i = wave.children.length;
+        while (i--) {
+            enemy = wave.children[i];
+            wave.remove(enemy);
+            cache.add(enemy);
+        }
+
+        wave.destroy();
+
+    }
+
+};
+
+// release the next enemy from cache
+round.release = function () {
+
+    if (this.cache.children.length > 0) {
+
+        var enemy = this.cache.children[0];
+        enemy.x = Math.floor(Math.random() * (this.game.world.width - 32));
+        enemy.y = -32;
+
+        this.cache.remove(enemy, false);
+
+        this.active.add(enemy);
+
+    }
+
+};
+
+// tick the active group, where the real action happens
+round.tick = function () {
+
+    var game = round.game;
+
+    this.active.forEach(function (enemy) {
+
+        enemy.y += 1;
+
+        if (enemy.y >= game.world.height) {
+
+            round.playerHP -= 5;
+
+            enemy.destroy();
+
+        }
+
+    });
+
+}
 
 var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
 
@@ -212,7 +212,7 @@ game.state.add('basics', {
         var parent = game.add.group();
         parent.name = 'parent';
 
-        // I can make another group nested, 
+        // I can make another group nested,
         var nested = game.add.group(parent);
         nested.name = 'nested';
 
@@ -222,5 +222,5 @@ game.state.add('basics', {
 
 });
 
-game.state.start('basics');
-//game.state.start('example-1');
+//game.state.start('basics');
+game.state.start('example-1');
