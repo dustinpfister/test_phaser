994540bdf91b0bf4302cd9de69e901833351b292
dustinpfister <dustin.pfister@gmail.com>
2018-08-29 09:04:42 -0400

    re worked phaser-state-loader to newer coding style

--- public/forpost/phaser-state-loader/main.js
+++ public/forpost/phaser-state-loader/main.js
@@ -1,76 +1,90 @@
-// th Boot State
-var Boot = {
- 
-    preload : function () {
- 
-        game.load.image('loadingbar', '/img/loadingbar.png');
- 
-    },
- 
-    // create method
-    create : function () {
- 
-        game.state.add('load', Load);
-        game.state.start('load');
- 
+var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
+
+// I always start off with a boot state
+// In this state I do anything that I want to get done
+// before even pre loading content
+game.state.add('boot', {
+
+    create: function () {
+
+        // start preload state
+        game.state.start('preload');
+
     }
- 
-};
- 
+
+});
+
+// pre-load any assets that will be used during the load state
+game.state.add('preload', {
+
+    preload: function () {
+
+        game.load.image('loadingbar', '/img/loadingbar.png');
+
+    },
+
+    // create method
+    create: function () {
+
+        // start the actual load state
+        game.state.start('load');
+
+    }
+
+});
+
 // the load state
-var Load = {
- 
-    preload : function () {
- 
-        // add a sprite that uses my loadingbar asset,
+game.state.add('load', {
+
+    preload: function () {
+
+        // add a sprite that uses my loading bar asset,
         // that was quickly loaded during the Boot Sate
         var loadSprite = game.add.sprite(0, 0, 'loadingbar');
         loadSprite.width = 0;
         loadSprite.x = game.world.centerX - loadSprite.width / 2;
         loadSprite.y = game.world.centerY - 16;
- 
+
         // what to do when a file as completed downloading
         game.load.onFileComplete.add(function (progress, key, success, loaded, total) {
- 
+
             loadSprite.width = game.width * (progress / 100);
             loadSprite.x = game.world.centerX - loadSprite.width / 2;
- 
+
             console.log('progress: ' + progress);
             console.log('key: ' + key);
             console.log('success: ' + success);
             console.log('loaded: ' + loaded + '\/' + total);
             console.log('**********');
- 
+
         }, this);
- 
+
         // start loading the asset files
         game.load.image('phaser', '/img/phaser.png');
- 
+
     },
- 
+
     // when done create will be called
-    create : function () {
- 
-        console.log('ready to rock!');
-        game.state.add('game', Game);
-        //game.state.start('game');
- 
+    create: function () {
+
+        // ready to start the game
+        game.state.start('game');
+
     }
- 
-};
- 
-// the actual Game state once everything is loaded
-var Game = {
- 
-    create : function () {
- 
+
+});
+
+// the actual game state
+game.state.add('game', {
+
+    create: function () {
+
         game.add.sprite(0, 0, 'phaser');
- 
-    },
- 
-    update : function () {}
- 
-};
- 
-// the main game variable
-var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', Boot);
+
+    }
+
+});
+
+
+// start the boot state
+game.state.start('boot');
