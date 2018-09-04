1159d221314cfdd006b4c56c545c75b33b92b0ad
dustinpfister <dustin.pfister@gmail.com>
2018-09-04 14:35:06 -0400

    make circle sheet

--- public/forpost/phaser-gameobj-enable-step/main.js
+++ public/forpost/phaser-gameobj-enable-step/main.js
@@ -1,10 +1,29 @@
+
+// make a basic sprite sheet
+var makeCircleSheet = function (game) {
+
+    var canvas = document.createElement('canvas');
+    ctx = canvas.getContext('2d');
+    canvas.width = 32;
+    canvas.height = 32;
+    ctx.strokeStyle = '#ff0000';
+    ctx.beginPath();
+    ctx.arc(16.5, 16.5, 15, 0, Math.PI * 2);
+    ctx.stroke();
+    this.game.cache.addSpriteSheet('sheet-circles', null, canvas, 32, 32, 1, 0, 0);
+    document.body.appendChild(canvas);
+
+};
+
 var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
 
 game.enableStep();
 
-game.state.add('example', {
+game.state.add('demo', {
 
     create: function () {
+
+        makeCircleSheet(this.game);
 
         this.game.input.onDown.add(function () {
 
@@ -22,4 +41,4 @@ game.state.add('example', {
 
 });
 
-game.state.start('example');
+game.state.start('demo');
