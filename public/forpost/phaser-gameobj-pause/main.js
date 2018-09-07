d141b52b385874ce89c0f6107bb710fc03d21e9c
dustinpfister <dustin.pfister@gmail.com>
2018-09-07 19:04:13 -0400

    paused demo

--- public/forpost/phaser-gameobj-pause/main.js
+++ public/forpost/phaser-gameobj-pause/main.js
@@ -1,28 +1,35 @@
 var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
 
 game.state.add('boot', {
-	
-	create : function(){
-		
-		
-		
-		game.state.start('demo');
-		
-	}
-	
-	
-});
 
+    create: function () {
+
+        game.state.start('demo');
+
+    }
+
+});
 
 game.state.add('demo', {
-	
-	create : function(){
-		
-		
-		
-	}
-	
-});
 
+    create: function () {
+
+        game.paused = true;
+
+    },
+
+    paused: function () {
+
+        console.log('yeah');
+
+    },
+
+    update: function () {
+
+        console.log('tick');
+
+    }
+
+});
 
 game.state.start('boot');
