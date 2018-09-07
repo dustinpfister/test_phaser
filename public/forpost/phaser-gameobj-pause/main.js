d6cde04dfb6a1c5d17d09534595876f0a713f1f8
dustinpfister <dustin.pfister@gmail.com>
2018-09-07 19:13:02 -0400

    addeding in sprites

--- public/forpost/phaser-gameobj-pause/main.js
+++ public/forpost/phaser-gameobj-pause/main.js
@@ -4,6 +4,19 @@ game.state.add('boot', {
 
     create: function () {
 
+        var canvas = document.createElement('canvas');
+        ctx = canvas.getContext('2d');
+        canvas.width = 32;
+        canvas.height = 32;
+        ctx.strokeStyle = '#ff0000';
+        ctx.fillStyle = '#ffff00';
+        ctx.beginPath();
+        ctx.arc(16.5, 16.5, 15, 0, Math.PI * 2);
+        ctx.stroke();
+        ctx.fill();
+
+        this.game.cache.addSpriteSheet('sheet', null, canvas, 32, 32, 1, 0, 0);
+
         game.state.start('demo');
 
     }
@@ -16,6 +29,15 @@ game.state.add('demo', {
 
         game.paused = true;
 
+        var i = 0,
+        count = 10,
+        sprite;
+        while (i < count) {
+            sprite = this.game.add.sprite(0, 0, 'sheet');
+            sprite.name = 'block' + i;
+            i += 1;
+        }
+
     },
 
     paused: function () {
@@ -26,7 +48,15 @@ game.state.add('demo', {
 
     update: function () {
 
-        console.log('tick');
+        var i = 0,
+        count = 10,
+        sprite;
+        while (i < count) {
+            sprite = this.game.world.getByName('block' + i);
+            sprite.x = game.world.centerX - 16;
+            sprite.y = game.world.centerY - 16;
+            i += 1;
+        }
 
     }
 
