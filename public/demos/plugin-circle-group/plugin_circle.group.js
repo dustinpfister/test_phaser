f4196b93056c7d6d36eafb9ec81b6d079f36aca2
dustinpfister <dustin.pfister@gmail.com>
2018-09-11 13:48:52 -0400

    new plugin demo

---
+++ public/demos/plugin-circle-group/plugin_circle.group.js
@@ -0,0 +1,8 @@
+Phaser.Plugin.CircleGroup = function (game, parent) {
+
+    Phaser.Plugin.call(this, game, parent);
+
+};
+
+Phaser.Plugin.CircleGroup.prototype = Object.create(Phaser.Plugin.prototype);
+Phaser.Plugin.CircleGroup.prototype.constructor = Phaser.Plugin.CircleGroup;
