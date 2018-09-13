339f01fb58f4bea58e3725b6172b5447d044c243
dustinpfister <dustin.pfister@gmail.com>
2018-09-13 19:45:05 -0400

    demo sprite-events-onkilled

---
+++ public/demos/sprite-events-onkilled/main.js
@@ -0,0 +1,28 @@
+
+var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
+
+game.state.add('boot', {
+	
+	create: function(){
+		
+		
+		game.state.start('demo');
+		
+	}
+	
+	
+});
+
+
+
+game.state.add('demo', {
+	
+	create: function(){
+		
+		
+	}
+	
+	
+});
+
+game.start.start('boot');
