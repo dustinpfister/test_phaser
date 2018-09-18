fbebbe69b27d5d759553e409ca647a6c86ade9df
dustinpfister <dustin.pfister@gmail.com>
2018-09-18 12:40:13 -0400

    renderTexture demo

--- public/forpost/phaser-sprite-data/basic.js
+++ public/forpost/phaser-sprite-data/basic.js
@@ -16,35 +16,45 @@ var mkSheet = function (game) {
 
 };
 
+var setupDataObject = function (game, sprite) {
+
+    // appending some values to the data object
+    sprite.data.frame = 0;
+    sprite.data.frame_max = 50;
+    sprite.data.sprite = sprite;
+    sprite.data.game = game;
+
+    // step sprite movement method
+    sprite.data.step = function () {
+
+        this.per = this.frame / this.frame_max;
+        this.bias = 1 - Math.abs(0.5 - this.per) / 0.5;
+
+        this.sprite.x = this.game.world.centerX - 16 - 100 + 200 * this.bias;
+        this.sprite.y = this.game.world.centerY - 16;
+
+        this.frame += 1;
+        this.frame %= this.frame_max;
+
+    };
+
+};
+
 var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
 
 game.state.add('basic', {
 
     create: function () {
 
+        // make my sprite sheet
         mkSheet(game);
 
+        // make a sprite using my sprite sheet
         var sprite = game.add.sprite(0, 0, 'sheet-block');
         sprite.name = 'bx';
 
-        sprite.data.frame = 0;
-        sprite.data.frame_max = 50;
-        sprite.data.sprite = sprite;
-        sprite.data.game = game;
-
-        // step sprite movement
-        sprite.data.step = function () {
-
-            this.per = this.frame / this.frame_max;
-            this.bias = 1 - Math.abs(0.5 - this.per) / 0.5;
-
-            this.sprite.x = this.game.world.centerX - 16 - 100 + 200 * this.bias;
-            this.sprite.y = this.game.world.centerY - 16;
-
-            this.frame += 1;
-            this.frame %= this.frame_max;
-
-        };
+        // seup the data object
+        setupDataObject(game, sprite);
 
     },
 
@@ -52,6 +62,7 @@ game.state.add('basic', {
 
         var sprite = game.world.getByName('bx');
 
+        // calling my step method
         sprite.data.step();
 
     }
