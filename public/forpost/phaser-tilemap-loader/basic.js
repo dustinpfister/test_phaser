a2700a8e5dab3823af46cd745cb9d064b19060e1
dustinpfister <dustin.pfister@gmail.com>
2018-09-26 10:56:34 -0400

    height in pixles

--- public/forpost/phaser-tilemap-loader/basic.js
+++ public/forpost/phaser-tilemap-loader/basic.js
@@ -1,6 +1,7 @@
 // load json and images
 var loadWorldData = function (game) {
     game.load.tilemap('map-world1', '/forpost/phaser-tilemap-loader/world2.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('image-blocks', '/img/sheet_blocks.png');
 };
 
 // load a give world number
@@ -10,20 +11,11 @@ var loadWorld = function (game, worldNum) {
 
     game.data = game.data || {};
 
-    var map = game.data.map = game.add.tilemap('map-world' + worldNum);
+    var map = game.data.map = game.add.tilemap('map-world1');
 
     map.addTilesetImage('blocks', 'image-blocks');
 
-};
-
-loadStage = function (game, stageNum) {
-
-    var map = game.data.map;
-
-    var layer = game.data.layer = map.createLayer('stage' + stageNum);
-    layer.fixedToCamera = false;
-    layer.x = 10;
-    layer.y = game.world.height - (layer.layer.heightInPixels)-10;
+    game.data.layer = map.createLayer('stage1');
 
 };
 
@@ -36,12 +28,10 @@ var displayMapProperties = function (game, textObj) {
 
 };
 
-game.state.add('boot', {
+
+game.state.add('basic', {
 
     preload: function () {
-
-        game.load.image('image-blocks', '/img/sheet_blocks.png');
-        game.load.image('image-guy', '/img/sheet_guy.png');
 
         loadWorldData(game);
 
@@ -51,10 +41,9 @@ game.state.add('boot', {
 
         // load World one
         loadWorld(game, 1);
-        loadStage(game, 1);
 
         // text display object
-        game.data.disp = game.add.text(10, 10, '', {
+        game.data.disp = game.add.text(10, 10,'', {
                 fill: 'white',
                 font: '15px courier'
             });
@@ -64,4 +53,3 @@ game.state.add('boot', {
 
 });
 
-game.state.start('boot');
