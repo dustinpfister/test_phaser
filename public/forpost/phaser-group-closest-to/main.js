abc7516ea8644199c25b81d9811fa0a2385128a2
dustinpfister <dustin.pfister@gmail.com>
2018-09-29 10:46:47 -0400

    scatter enemies

--- public/forpost/phaser-group-closest-to/main.js
+++ public/forpost/phaser-group-closest-to/main.js
@@ -4,8 +4,8 @@ var mkSheet = function (game) {
     // basic block sprite sheet, made with canvas
     var canvas = document.createElement('canvas');
     ctx = canvas.getContext('2d');
-    canvas.width = 32;
-    canvas.height = 96;
+    canvas.width = 96;
+    canvas.height = 32;
 
     // player block
     ctx.fillStyle = '#0000ff';
@@ -20,7 +20,7 @@ var mkSheet = function (game) {
     ctx.lineWidth = 3;
     ctx.strokeStyle = '#ffffff';
     ctx.fillRect(64, 0, 32, 32);
-    ctx.strokeRect(65, 1, 31, 31);
+    ctx.strokeRect(65.5, 1.5, 29, 29);
 
     game.cache.addSpriteSheet('sheet-blocks', null, canvas, 32, 32, 3, 0, 0);
 
@@ -33,8 +33,24 @@ game.state.add('boot', {
     create: function () {
 
         mkSheet(game);
-		
-		var sprite = game.add.sprite(0,0,'sheet-blocks',0);
+
+        game.data = game.data || {};
+
+        var enemies = game.data.enemies = game.add.group();
+        var i = 0,
+        enemy,
+        len = 15;
+        while (i < len) {
+
+            enemies.create(0, 0, 'sheet-blocks', 1);
+
+            i += 1;
+
+        }
+
+        enemies.scatter(new Phaser.Rectangle(0, 0, game.world.width - 32, game.world.height - 32));
+
+        var player = game.data.player = game.add.sprite(0, 0, 'sheet-blocks', 0);
 
     },
 
