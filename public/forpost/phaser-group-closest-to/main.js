e4909ba99b207c9aabb85fca923317e38b47af45
dustinpfister <dustin.pfister@gmail.com>
2018-09-29 10:32:07 -0400

    mk sheet funtion

--- public/forpost/phaser-group-closest-to/main.js
+++ public/forpost/phaser-group-closest-to/main.js
@@ -1,9 +1,40 @@
+
+var mkSheet = function (game) {
+
+    // basic block sprite sheet, made with canvas
+    var canvas = document.createElement('canvas');
+    ctx = canvas.getContext('2d');
+    canvas.width = 32;
+    canvas.height = 96;
+
+    // player block
+    ctx.fillStyle = '#0000ff';
+    ctx.fillRect(0, 0, 32, 32);
+
+    // enemy block
+    ctx.fillStyle = '#ff0000';
+    ctx.fillRect(32, 0, 32, 32);
+
+    // enemy selected block
+    ctx.fillStyle = '#ff0000';
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = '#ffffff';
+    ctx.fillRect(64, 0, 32, 32);
+    ctx.strokeRect(65, 1, 31, 31);
+
+    game.cache.addSpriteSheet('sheet-blocks', null, canvas, 32, 32, 3, 0, 0);
+
+};
+
 var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
 
 game.state.add('boot', {
 
     create: function () {
 
+        mkSheet(game);
+		
+		var sprite = game.add.sprite(0,0,'sheet-blocks',0);
 
     },
 
