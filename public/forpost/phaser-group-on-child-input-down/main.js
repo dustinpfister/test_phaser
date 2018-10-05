7353fa15a7535c78567b72a6f4fe3e98eca4da16
dustinpfister <dustin.pfister@gmail.com>
2018-10-05 15:08:10 -0400

    phaser-group-on-child-input-down

---
+++ public/forpost/phaser-group-on-child-input-down/main.js
@@ -0,0 +1,19 @@
+var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
+
+
+game.state.add('demo', {
+
+    create: function () {
+
+
+    },
+
+    // update loop
+    update: function () {
+
+
+    }
+
+});
+
+game.state.start('demo');
