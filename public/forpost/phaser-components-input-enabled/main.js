6466de21813108f99958ef8205e5d64f7d007954
dustinpfister <dustin.pfister@gmail.com>
2018-10-06 12:39:21 -0400

    broke demo down

--- public/forpost/phaser-components-input-enabled/main.js
+++ public/forpost/phaser-components-input-enabled/main.js
@@ -1,40 +1,57 @@
+
+var mkGraphic = function (game) {
+
+    var gra = game.data.gra = game.add.graphics(160, 120);
+
+    // enable the input
+    gra.inputEnabled = true;
+
+};
+
+var draw = function (gra, color) {
+
+    color = color || 0x0000ff;
+
+    gra.clear();
+    gra.beginFill(color);
+    gra.drawRect(-50, -50, 100, 100);
+    gra.endFill();
+
+};
+
+var attachEvents = function () {
+
+    var gra = game.data.gra;
+
+    // I can now use the onInputDown event
+    gra.events.onInputDown.add(function (gra, pt) {
+
+        draw(gra, 0x00ff00);
+
+    });
+
+    // onInputUp also
+    gra.events.onInputUp.add(function (gra, pt) {
+
+        draw(gra, 0x0000ff);
+
+    });
+
+};
+
 var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
 
 game.state.add('demo', {
 
     create: function () {
 
-        var gra = game.add.graphics(160, 120),
+        game.data = game.data || {};
 
-        draw = function (gra, color) {
+        mkGraphic(game);
 
-            color = color || 0x0000ff;
+        attachEvents();
 
-            gra.clear();
-            gra.beginFill(color);
-            gra.drawRect(-50, -50, 100, 100);
-            gra.endFill();
-
-        };
-
-        // enable the input
-        gra.inputEnabled = true;
-
-        // I can now use the onInputDown event
-        gra.events.onInputDown.add(function (gra, pt) {
-
-            draw(gra, 0x00ff00);
-
-        });
-
-        // onInputUp also
-        gra.events.onInputUp.add(function (gra, pt) {
-
-            draw(gra, 0x0000ff);
-
-        });
-
-        draw(gra);
+        draw(game.data.gra);
     }
 
 });
