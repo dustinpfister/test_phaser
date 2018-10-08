4185d06b19d9930bbbae43bb688edb8aea08cca0
dustinpfister <dustin.pfister@gmail.com>
2018-10-07 20:38:45 -0400

    started a canvas grid solution

--- public/forpost/phaser-create-grid/main.js
+++ public/forpost/phaser-create-grid/main.js
@@ -1,7 +1,41 @@
 
+/*
 var createGrid = function (game, done) {
 
-    game.create.grid('', 320, 240, 32, 24, 'red', true, done);
+game.create.grid('', 320, 240, 32, 24, 'red', true, done);
+
+};
+ */
+
+createGridCanvas = function (opt) {
+
+    opt = opt || {};
+
+    opt.key = opt.key || 'grid';
+    opt.pxWidth = opt.pxWidth || 320;
+    opt.pxHeight = opt.pxHeight || 240;
+    opt.cellWidth = opt.cellWidth || 32;
+    opt.cellHeight = opt.cellHeight || 24;
+
+    var y = 0,
+    x,
+    w = opt.pxWidth / opt.cellWidth,
+    h = opt.pxHeight / opt.cellHeight;
+
+    while (y < opt.cellHeight) {
+
+        x = 0;
+        while (x < opt.cellWidth) {
+
+            ctx.strokeRect(x, y, w, h);
+
+            x += 1;
+
+        }
+
+        y += 1;
+
+    }
 
 };
 
@@ -11,12 +45,17 @@ game.state.add('boot', {
 
     create: function () {
 
+	/*
         createGrid(game, function (texture) {
 
             game.add.sprite(0, 0, texture);
 
         });
 
+		*/
+		
+		
+		
     }
 
 });
