e3e0651c3868728352d764b928f0d0c029b5b4ad
dustinpfister <dustin.pfister@gmail.com>
2018-10-09 18:03:01 -0400

    helpers

--- public/forpost/phaser-plugins/plugin_ball.js
+++ public/forpost/phaser-plugins/plugin_ball.js
@@ -3,9 +3,8 @@ var Plugin_ball = function (game, opt) {
 
     var plug = new Phaser.Plugin(game, game.plugins);
 
+    // Ball SPRITE SHEET
     var createBallSheet = function (game) {
-
-        // Ball SPRITE SHEET
         var canvas = document.createElement('canvas'),
         ctx = canvas.getContext('2d');
         canvas.width = 16;
@@ -13,7 +12,20 @@ var Plugin_ball = function (game, opt) {
         ctx.fillStyle = 'white';
         ctx.fillRect(0, 0, 16, 16);
         game.cache.addSpriteSheet('sheet-ball', null, canvas, 16, 16, 1, 0, 0);
+    };
 
+    // BALL SPRITE
+    var createBallSprite = function (game) {
+        var x = game.world.width / 2,
+        y = 32,
+        ball = game.data.ball.sprite = game.add.sprite(x, y, 'sheet-ball');
+        ball.anchor.set(0.5, 0.5);
+        // physics
+        game.physics.enable(ball);
+        ball.body.collideWorldBounds = true;
+        ball.checkWorldBounds = true;
+        ball.body.velocity.set(100, 50);
+        ball.body.bounce.set(1);
     };
 
     // call once
@@ -28,17 +40,7 @@ var Plugin_ball = function (game, opt) {
 
         createBallSheet(game);
 
-        // BALL SPRITE
-        var x = game.world.width / 2,
-        y = 32,
-        ball = game.data.ball.sprite = game.add.sprite(x, y, 'sheet-ball');
-        ball.anchor.set(0.5, 0.5);
-        // physics
-        game.physics.enable(ball);
-        ball.body.collideWorldBounds = true;
-        ball.checkWorldBounds = true;
-        ball.body.velocity.set(100, 50);
-        ball.body.bounce.set(1);
+        createBallSprite(game);
 
     };
 
