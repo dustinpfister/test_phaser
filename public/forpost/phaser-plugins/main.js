20b2f6f09ad9470069bf25afbc412e5df91b8da8
dustinpfister <dustin.pfister@gmail.com>
2018-10-09 12:03:12 -0400

    first plugins init method works

--- public/forpost/phaser-plugins/main.js
+++ public/forpost/phaser-plugins/main.js
@@ -1,18 +1,40 @@
-var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
 
+var myPlugin = function (game, opt) {
+
+    var plugin = new Phaser.Plugin(game, game.plugins);
+
+    plugin.init = function (opt) {
+
+        console.log('hello I am a plugin');
+        console.log(opt.foo); // 'bar'
+
+    };
+
+    game.plugins.add(plugin, opt);
+
+};
+
+var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
 
 game.state.add('demo', {
 
     create: function () {
 
+        myPlugin(game, {
+            foo: 'bar'
+        });
+
+        /*
+        game.plugins.add(myPlugin, {
+        foo: 'bar'
+        });
+
+         */
 
     },
 
     // update loop
-    update: function () {
-
-
-    }
+    update: function () {}
 
 });
 
