c25f6c60595dcb239717b402633c5f5b0c842a43
dustinpfister <dustin.pfister@gmail.com>
2018-10-10 12:42:17 -0400

    create ball pool helper

--- public/forpost/phaser-sprite-lifespan/main.js
+++ public/forpost/phaser-sprite-lifespan/main.js
@@ -1,15 +1,24 @@
 
+var createBallSpritePool = function (game) {
+
+    game.data = game.data || {};
+
+    var ballPool = game.data.ballPool = game.add.group();
+    i = 0,
+    len = 10;
+    while (i < len) {
+        ballPool.create(0, 0, 'sheet-ball', 0);
+        i += 1;
+    }
+
+};
 
 var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
 
 game.state.add('boot', {
 
-    create: function () {
-
-
-    }
+    create: function () {}
 
 });
-
 
 game.state.start('boot');
