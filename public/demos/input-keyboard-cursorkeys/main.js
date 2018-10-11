62dffa2663fc569a2cf68b4007708298018b4cd9
dustinpfister <dustin.pfister@gmail.com>
2018-10-11 18:03:55 -0400

    ball sheet

--- public/demos/input-keyboard-cursorkeys/main.js
+++ public/demos/input-keyboard-cursorkeys/main.js
@@ -1,14 +1,33 @@
 
+var createBallSheet = function (game) {
+
+    var canvas = document.createElement('canvas');
+    ctx = canvas.getContext('2d');
+
+    canvas.width = 32;
+    canvas.height = 32;
+
+    ctx.fillStyle = '#ffffff';
+    ctx.beginPath();
+    ctx.closePath();
+    ctx.arc(16, 16, 12, 0, Math.PI * 2);
+    ctx.fill();
+
+    game.cache.addSpriteSheet('sheet-ball', null, canvas, 32, 32, 1, 0, 0);
+};
+
 var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
 
 game.state.add('boot', {
 
     create: function () {
 
+        createBallSheet(game);
+
+        var sprite = game.add.sprite(0, 0, 'sheet-ball');
 
     }
 
 });
-
 
 game.state.start('boot');
