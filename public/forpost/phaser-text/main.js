3298fed07297043f145fa3a77e81bfb0ef6eb759
dustinpfister <dustin.pfister@gmail.com>
2018-10-20 09:45:15 -0400

    update text

--- public/forpost/phaser-text/main.js
+++ public/forpost/phaser-text/main.js
@@ -18,4 +18,29 @@ game.state.add('text-basic', {
 
 });
 
-game.state.start('text-basic');
+game.state.add('text-update', {
+
+    // create method
+    create: function () {
+        var data = game.data = {
+            foos: 0
+        };
+        data.disp = game.add.text(20, 20, '', {
+                fill: '#ffffff',
+                font: '15px courier'
+            });
+    },
+
+    // the update method will be called on each tick
+    update: function () {
+
+        // a text objects text can be changed via the text property
+        var data = game.data;
+        data.disp.text = 'foos: ' + data.foos;
+        data.foos += 1;
+
+    }
+
+});
+
+game.state.start('text-update');
