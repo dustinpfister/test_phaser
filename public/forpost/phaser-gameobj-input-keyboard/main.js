c4df0c42152280406bb76aadb13c62108c6396de
dustinpfister <dustin.pfister@gmail.com>
2018-10-21 17:52:49 -0400

    movement

--- public/forpost/phaser-gameobj-input-keyboard/main.js
+++ public/forpost/phaser-gameobj-input-keyboard/main.js
@@ -4,16 +4,45 @@ game.state.add('demo', {
 
     create: function () {
 
-        var guy = game.add.sprite(0, 0, 'sheet-guy');
+        game.data = game.data || {};
+
+        var data = game.data;
+
+        var guy = data.guy = game.add.sprite(0, 0, 'sheet-guy');
 
         game.physics.enable(guy);
-
-        guy.body.gravity.set(0, 100);
+        guy.body.gravity.set(0, 200);
         guy.body.collideWorldBounds = true;
+
+        data.cursors = game.input.keyboard.createCursorKeys();
+
+        // making jumps event driven
+        data.cursors.up.onDown.add(function () {
+            if (guy.body.onFloor()) {
+                guy.body.velocity.y = -150;
+            }
+        });
 
     },
 
-    update: function () {};
+    update: function () {
+
+        var data = game.data,
+        cursors = data.cursors,
+        guy = data.guy;
+
+        guy.body.velocity.x = 0;
+
+        //  polling for left and right movement
+        if (cursors.left.isDown) {
+            guy.body.velocity.x = -150;
+        }
+
+        if (cursors.right.isDown) {
+            guy.body.velocity.x = 150;
+        }
+
+    }
 
 });
 
