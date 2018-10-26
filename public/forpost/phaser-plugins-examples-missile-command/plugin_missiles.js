b05d5cf3f21c3b9e52ece96b7db5df27b9c7cca8
dustinpfister <dustin.pfister@gmail.com>
2018-10-26 13:58:55 -0400

    missile class started

--- public/forpost/phaser-plugins-examples-missile-command/plugin_missiles.js
+++ public/forpost/phaser-plugins-examples-missile-command/plugin_missiles.js
@@ -1,6 +1,44 @@
 
 var Plugin_missiles = function (game, opt) {
 
+    var Missile = function (sprite) {
+
+        this.sprite = sprite;
+        this.set('a', 0, 0, 10, 10);
+
+    };
+
+    // set a missle to player 'p' or ai 'a', and start and target positions
+    Missile.prototype.set = function (faction, sx, sy, tx, ty) {
+
+        var sprite = this.sprite;
+
+        this.faction = faction || 'a';
+        this.sx = sx === undefined ? 0 : sx;
+        this.sy = sy === undefined ? 0 : sy;
+        this.tx = tx === undefined ? 100 : tx;
+        this.ty = ty === undefined ? 100 : ty;
+
+        this.launched = false;
+
+        sprite.x = this.sx;
+        sprite.y = this.sy;
+        sprite.anchor.set(0.5, 0.5);
+
+        // a set missile starts out killed
+        // Missle.launch must be called to revive
+        sprite.kill();
+
+    };
+
+    // launch a set missile
+    Missile.prototype.launch = function () {
+
+        this.launched = true;
+        this.sprite.revive();
+
+    };
+
     var createMissilePool = function (game) {
 
         var data = game.data,
@@ -11,11 +49,11 @@ var Plugin_missiles = function (game, opt) {
         data.missiles = game.add.group();
         while (i < len) {
 
-            missile = game.make.graphics(0, 0);
+            sprite = game.make.sprite(0, 0, 'sheet-missile');
 
-            missile.data = {}
+            sprite.data = new Missile(sprite);
 
-            data.missiles.add(missile);
+            data.missiles.add(sprite);
 
             i += 1;
 
