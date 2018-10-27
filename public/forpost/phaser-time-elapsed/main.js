b4f934dfe0f33ce92b7ddebc813099e3c3b37a96
dustinpfister <dustin.pfister@gmail.com>
2018-10-26 21:14:42 -0400

    clean up

--- public/forpost/phaser-time-elapsed/main.js
+++ public/forpost/phaser-time-elapsed/main.js
@@ -1,20 +1,15 @@
 
 
 var makeSprite = function (game) {
-
     var sprite = game.data.sprite = game.add.sprite(10, 10, 'sheet-box');
-
     // setting a pixels per second value for sprite.data
     sprite.data.pps = 128;
-
     // setting an update method for the sprite
     sprite.update = function () {
-
         // using game.time.elapsed to figure deltaX
         var deltaX = game.time.elapsed / 1000 * sprite.data.pps;
         sprite.x = Phaser.Math.wrap(sprite.x += deltaX, -8, game.world.width + 8);
     };
-
 };
 
 var createBoxSheet = function (game) {
@@ -29,25 +24,11 @@ var createBoxSheet = function (game) {
 
 // the Phaser game instance
 var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
-
 game.state.add('pps', {
-
     create: function () {
-
         game.data = {};
-
         createBoxSheet(game);
-
         makeSprite(game);
-
-    },
-
-    update: function () {
-
-        var sprite = game.data.sprite;
-
     }
-
 });
-
 game.state.start('pps');
