21545e8b57a26d6c4b2881f93bc73816751c08fd
dustinpfister <dustin.pfister@gmail.com>
2018-10-30 16:34:15 -0400

    cannon sheet

--- public/forpost/phaser-physics-drag/main.js
+++ public/forpost/phaser-physics-drag/main.js
@@ -1,8 +1,13 @@
 
-var createLaunchLines = function (game) {
+var createLaunchLines = function (game, launcher) {
 
     var gfx = game.add.graphics();
     gfx.fixedToCamera = true;
+
+    var launch = game.data.launch = {};
+
+    launch.lanucher = launcher;
+    launch.angle = 0;
 
 };
 
@@ -20,6 +25,16 @@ var createBallSheet = function (game) {
     game.cache.addSpriteSheet('sheet-ball', null, canvas, 32, 32, 1, 0, 0);
 };
 
+var createCannonSheet = function (game) {
+    var canvas = document.createElement('canvas');
+    ctx = canvas.getContext('2d');
+    canvas.width = 32;
+    canvas.height = 32;
+    ctx.fillStyle = '#8f0000';
+    ctx.fillRect(0, 0, 32, 32);
+    game.cache.addSpriteSheet('sheet-cannon', null, canvas, 32, 32, 1, 0, 0);
+};
+
 var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
 
 game.state.add('ball-bounce', {
@@ -29,9 +44,11 @@ game.state.add('ball-bounce', {
         game.data = {};
 
         createBallSheet(game);
+        createCannonSheet(game);
 
         var ball = game.data.ball = game.add.sprite(0, 0, 'sheet-ball', 0);
         ball.anchor.set(0.5, 0.5);
+        ball.kill();
 
         // enable physics
         game.physics.enable(ball);
