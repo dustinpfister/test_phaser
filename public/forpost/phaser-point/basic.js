c7f5959403c41171a0e3634295df1c0385c50ac6
dustinpfister <dustin.pfister@gmail.com>
2018-11-03 13:55:23 -0400

    basic example

---
+++ public/forpost/phaser-point/basic.js
@@ -0,0 +1,10 @@
+
+var pt = new Phaser.Point(10, 10);
+
+// so then there are the x and y properties of the point
+console.log(pt.x, pt.y); // 15, 20
+
+// and then there are methods that can be used with other points
+var pt2 = new Phaser.Point(20, 20),
+angle = pt.angle(pt2);
+console.log(angle / Math.PI * 180); // 45
