048cc0c43936dbc436531363a294fe43481e982d
dustinpfister <dustin.pfister@gmail.com>
2018-11-08 13:18:46 -0500

    mkGroup

--- public/forpost/phaser-physics-immovable/main.js
+++ public/forpost/phaser-physics-immovable/main.js
@@ -6,7 +6,33 @@ var mkImmovable = function (game) {
     gfx.beginFill(0x00ff00);
     gfx.drawRect(-32, -32, 64, 64);
 
-}
+};
+
+var mkGroup = function (game) {
+
+    var group = game.add.group();
+
+    var i = 0,
+    len = 10;
+    while (i < len) {
+
+        var radian = Math.PI * 2 / len * i,
+        dist = Math.random() * 30 + 70;
+
+        var gfx = game.make.graphics(
+                game.world.centerX + Math.cos(radian) * dist,
+                game.world.centerY + Math.sin(radian) * dist);
+
+        gfx.beginFill(0x00ff00);
+        gfx.drawRect(-8, -8, 16, 16);
+
+        group.add(gfx);
+
+        i += 1;
+
+    }
+
+};
 
 var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
 
@@ -17,6 +43,8 @@ game.state.add('demo', {
         game.data = {};
 
         mkImmovable(game);
+
+        mkGroup(game);
 
     },
 
