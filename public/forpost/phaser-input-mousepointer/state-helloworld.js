e3002e321721e041a34a79f17208be574e515342
dustinpfister <dustin.pfister@gmail.com>
2018-11-09 17:27:16 -0500

    mousedown

--- public/forpost/phaser-input-mousepointer/state-helloworld.js
+++ public/forpost/phaser-input-mousepointer/state-helloworld.js
@@ -1,7 +1,3 @@
-
-
-var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
-
 game.state.add('hello-world', {
 
     render: function () {
@@ -11,5 +7,3 @@ game.state.add('hello-world', {
     }
 
 });
-
-game.state.start('hello-world');
---
+++ public/forpost/phaser-input-mousepointer/state-mousedown.js
@@ -0,0 +1,38 @@
+game.state.add('mouse-down', {
+
+    create: function () {
+
+        var st = {
+            fill: '#ffffff',
+            font: '15px courier'
+        };
+
+        game.add.text(20, 20, 'tx0', st);
+        game.add.text(20, 40, 'tx1', st);
+
+    },
+
+    update: function () {
+
+        var pt = game.input.mousePointer,
+        time,
+        tx = game.world.children;
+
+        if (pt.isDown) {
+
+            // time can be found thanks to pt.timeDown
+            time = new Date() - pt.timeDown;
+
+            tx[0].text = 'mouse is down!';
+            tx[1].text = time;
+
+        } else {
+
+            tx[0].text = 'mouse is not down.';
+            tx[1].text = '';
+
+        }
+
+    }
+
+});
