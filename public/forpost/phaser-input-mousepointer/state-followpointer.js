3ccbca96843a750d86b1a4ad7c121b0c74200057
dustinpfister <dustin.pfister@gmail.com>
2018-11-09 17:44:40 -0500

    following pointer

--- public/forpost/phaser-input-mousepointer/state-followpointer.js
+++ public/forpost/phaser-input-mousepointer/state-followpointer.js
@@ -1,24 +1,39 @@
 var followPointer = function (game, sprite) {
 
-    var mouse = game.input.mousePointer;
+    // the mouse pointer
+    var pt = game.input.mousePointer,
 
-    sprite.body.velocity.set(0, 0);
+    // angle to pointer
+    angle = sprite.position.angle(pt);
 
-}
+    sprite.body.velocity.set(Math.cos(angle) * 10, Math.sin(angle) * 10);
+
+};
 
 game.state.add('follow-pointer', {
 
     create: function () {
 
         var data = game.data = {},
-        sprite = game.data.sprite = game.add.sprite(0, 0, 'sheet-ball');
+        sprite = game.data.sprite = game.add.sprite(game.world.centerX, game.world.centerY, 'sheet-ball'),
+        tx = game.data.tx = game.add.text(10, 10, 0, {
+                fill: 'white',
+                font: '10px courier'
+            });
+
+        sprite.anchor.set(0.5, 0.5);
 
         game.physics.enable(sprite);
 
     },
     update: function () {
 
-        followPointer(game, game.data.sprite);
+        var pt = game.input.mousePointer,
+        sprite = game.data.sprite;
+
+        game.data.tx.text = sprite.position.angle(pt) / Math.PI * 180;
+
+        followPointer(game, sprite);
 
     }
 
