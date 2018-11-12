20f36d682b5ada0cafc2c7a2adc967993b1b0c3d
dustinpfister <dustin.pfister@gmail.com>
2018-11-12 11:37:27 -0500

    renamed plugin

--- public/forpost/phaser-plugins-examples-defence/main.js
+++ public/forpost/phaser-plugins-examples-defence/main.js
@@ -1,11 +1,24 @@
 
+
+var createSheetBuildings = function (game) {
+    var canvas = document.createElement('canvas'),
+    ctx = canvas.getContext('2d');
+    canvas.width = 32;
+    canvas.height = 32;
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, 16, 32);
+    game.cache.addSpriteSheet('sheet-buildings', null, canvas, 16, 32, 1, 0, 0);
+};
+
 var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
 
 game.state.add('demo', {
 
     create: function () {
 
-        Plugin_buildings(game, {});
+        createSheetBuildings(game);
+
+        Plugin_buildings(game);
 
     }
 
--- public/forpost/phaser-plugins-examples-defence/plugin_buildings.js
+++ public/forpost/phaser-plugins-examples-defence/plugin_buildings.js
@@ -1,17 +1,9 @@
 
-var Plugin_runner = function (game, opt) {
+var Plugin_buildings = function (game, opt) {
 
-    // Guy SPRITE SHEET
-    var createGuySheet = function (game) {
-        var canvas = document.createElement('canvas'),
-        ctx = canvas.getContext('2d');
-        canvas.width = 16;
-        canvas.height = 32;
-        ctx.fillStyle = 'white';
-        ctx.fillRect(0, 0, 16, 32);
-        game.cache.addSpriteSheet('sheet-guy', null, canvas, 16, 32, 1, 0, 0);
-    };
+    opt = opt || {};
 
+    opt.sheetKey = opt.sheetKey || 'sheet-buildings';
 
     // The plugin Object
     var plug = new Phaser.Plugin(game, game.plugins);
