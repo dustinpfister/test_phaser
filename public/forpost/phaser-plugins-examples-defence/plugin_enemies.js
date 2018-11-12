293b4b86c701c9f5139bd43c8c5d4de90951a6aa
dustinpfister <dustin.pfister@gmail.com>
2018-11-12 15:07:38 -0500

    createEnemiesGroup

--- public/forpost/phaser-plugins-examples-defence/plugin_enemies.js
+++ public/forpost/phaser-plugins-examples-defence/plugin_enemies.js
@@ -13,51 +13,42 @@ var Plugin_enemies = function (game, opt) {
     layer;
 
     var setSpawnTiles = function (map) {
-
         map.forEach(function (tile, i) {
-
             var props = tile.properties;
-
             if (props) {
-
                 if (props.spawn) {
-
                     spawnTiles.push(tile);
-
                 }
             }
-
         });
-
-        console.log(spawnTiles);
-
     };
+
+    var createEnemiesGroup = function (game) {
+
+        // create enemies group
+        var enemies = game.data.enemies = game.add.group();
+        var i = 10,
+        enemy;
+        while (i--) {
+            enemy = enemies.create(-32, 0, opt.sheetKey, 0);
+            enemy.kill();
+            enemies.add(enemy);
+        }
+
+    }
 
     // call once
     plug.init = function (opt) {
 
         // create or append game.data
         game.data = game.data || {};
-		
-		
+
         map = opt.map;
         layer = map.layers[map.getLayer('layer-gameboard')];
 
         setSpawnTiles(map);
-		
-		
-		// create enemies group
-		
-		var enemies = game.data.enemies = game.add.group();
-        
-		var i = 10;
-		while(i--){
-			
-			
-			
-		}
 
-        //var enemy = game.add.sprite(32, 0, 'sheet-enemies', 0);
+        createEnemiesGroup(game);
 
     };
 
