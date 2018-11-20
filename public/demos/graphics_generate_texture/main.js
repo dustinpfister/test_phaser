91a2c69c32ef05436b541eb61261682e21f8b64b
dustinpfister <dustin.pfister@gmail.com>
2018-11-20 14:25:06 -0500

    started new demo

---
+++ public/demos/graphics_generate_texture/main.js
@@ -0,0 +1,12 @@
+var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
+
+game.state.add('demo', {
+
+    // create method
+    create: function () {},
+
+    update: function () {}
+
+});
+
+game.state.start('demo');
