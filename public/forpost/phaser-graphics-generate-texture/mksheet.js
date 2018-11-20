afab34b2c0d5393ad9ad7d510b5f58774ff54d16
dustinpfister <dustin.pfister@gmail.com>
2018-11-20 17:14:51 -0500

    creating sprite sheet with graphics

--- public/forpost/phaser-graphics-generate-texture/mksheet.js
+++ public/forpost/phaser-graphics-generate-texture/mksheet.js
@@ -1,39 +1,44 @@
 var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
 
-game.state.add('res', {
+game.state.add('mksheet', {
 
     // create method
     create: function () {
 
         // create graphics
-        var gfx = game.add.graphics(0, 0);
+        var gfx = game.make.graphics(0, 0);
         gfx.lineStyle(3, 0xffffff, 1);
+        gfx.visible = false;
+
+        // draw frame 0
         gfx.moveTo(16, 3);
         gfx.lineTo(29, 29);
         gfx.lineTo(3, 29);
         gfx.currentPath.shape.closed = true;
-        gfx.visible = false;
 
-        // create textures with different resolutions
-        var i = 0,
-        sprite,
-        texture,
-        len = 9;
-        while (i < len) {
+        // draw frame 1
+        gfx.moveTo(48, 29);
+        gfx.lineTo(35, 3);
+        gfx.lineTo(61, 3);
+        gfx.currentPath.shape.closed = true;
 
-            texture = gfx.generateTexture(.15 + 0.85 * (i / len));
-            game.add.sprite(0 + 32 * i, 32, texture);
+        // creating a sprite sheet with generateTexture
+        game.cache.addSpriteSheet('sheet', null, gfx.generateTexture().baseTexture.source, 32, 32, 2, 0, 0);
 
-            i += 1;
+        // creating a sprite with the sheet
+        var sprite = game.add.sprite(game.world.centerX, game.world.centerY, 'sheet', 0);
+        sprite.anchor.set(0.5, 0.5);
+        sprite.width = 100;
+        sprite.height = 100;
 
-        }
-
-        game.cache.addSpriteSheet('sheet', null, gfx.generateTexture().baseTexture.source, 32, 32, 1, 0, 0);
-
-        game.add.sprite(32, 100, 'sheet');
+        // step frame index every second
+        game.time.events.loop(1000, function () {
+            sprite.frame += 1;
+            sprite.frame = sprite.frame %= 2;
+        });
 
     }
 
 });
 
-game.state.start('res');
+game.state.start('mksheet');
