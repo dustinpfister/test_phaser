428a70ef8b62d4c032698469ccb8d6f6fbc25706
dustinpfister <dustin.pfister@gmail.com>
2018-11-20 15:17:56 -0500

    resalution demo

--- public/forpost/phaser-graphics-generate-texture/res.js
+++ public/forpost/phaser-graphics-generate-texture/res.js
@@ -1,29 +1,35 @@
 var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
 
-game.state.add('basic', {
+game.state.add('res', {
 
     // create method
     create: function () {
 
-        var data = game.data = {};
-
-        var gfx = data.gfx = game.add.graphics(0, 0);
-
-        //gfx.beginFill(0x00ff00);
+        // create graphics
+        var gfx = game.add.graphics(0, 0);
         gfx.lineStyle(3, 0xffffff, 1);
-        gfx.moveTo(8, 0);
-        gfx.lineTo(16, 16);
-        gfx.lineTo(0, 16);
+        gfx.moveTo(16, 0);
+        gfx.lineTo(32, 32);
+        gfx.lineTo(0, 32);
         gfx.currentPath.shape.closed = true;
-
         gfx.visible = false;
 
-        var texture = gfx.generateTexture(2);
+        // create textures with different resolutions
+        var i = 0,
+        sprite,
+        texture,
+        len = 9;
+        while (i < len) {
 
-        var sprite = game.add.sprite(32, 32, texture);
+            texture = gfx.generateTexture(.15 + 0.85 * (i / len));
+            game.add.sprite(0 + 32 * i, 32, texture);
+
+            i += 1;
+
+        }
 
     }
 
 });
 
-game.state.start('basic');
+game.state.start('res');
