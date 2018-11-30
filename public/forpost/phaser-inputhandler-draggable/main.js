653fbdba4ae06f936aa145febc69faa9f59bad25
dustinpfister <dustin.pfister@gmail.com>
2018-11-30 10:40:57 -0500

    basic demo

--- public/forpost/phaser-inputhandler-draggable/main.js
+++ public/forpost/phaser-inputhandler-draggable/main.js
@@ -1,21 +1,21 @@
-var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea', 
- 
-{
- 
-        // create the sprite
-        create : function () {
- 
-            var bx = game.add.graphics(game.world.centerX, game.world.centerY);
- 
-            bx.beginFill(0xff0000);
-            bx.drawRect(-50, -50, 100, 100);
-            bx.endFill();
- 
-            bx.inputEnabled = true;
-            bx.input.draggable = true;
- 
-        }
- 
+var game = new Phaser.Game(320, 240, Phaser.AUTO, 'gamearea');
+
+game.state.add('basic', {
+
+    // create the sprite
+    create: function () {
+
+        var bx = game.add.graphics(game.world.centerX, game.world.centerY);
+
+        bx.beginFill(0xff0000);
+        bx.drawRect(-50, -50, 100, 100);
+        bx.endFill();
+
+        bx.inputEnabled = true;
+        bx.input.draggable = true;
+
     }
- 
-);
+
+});
+
+game.state.start('basic');
