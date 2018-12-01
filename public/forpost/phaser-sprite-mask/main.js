6a11e745de68b666a2744025b2274f08bec24a6a
dustinpfister <dustin.pfister@gmail.com>
2018-11-30 22:11:20 -0500

    using a group

--- public/forpost/phaser-sprite-mask/main.js
+++ public/forpost/phaser-sprite-mask/main.js
@@ -25,7 +25,7 @@ game.state.add('mask-demo', {
 
     create: function () {
 
-        var texture = game.make.graphics(0,0);
+        var texture = game.make.graphics(0, 0);
         texture.beginFill(0xff0000);
         texture.drawRect(0, 0, 32, 32);
         texture.endFill();
@@ -36,7 +36,15 @@ game.state.add('mask-demo', {
 
         //Here we add a Sprite to the display list
         sprite = game.add.sprite(0, 0, texture.generateTexture());
+        sprite.scale.set(2);
         sprite.mask = mask;
+
+        var group = game.add.group();
+        group.add(sprite);
+        group.add(mask);
+
+        group.x = 32;
+        group.y = 32;
 
     }
 
